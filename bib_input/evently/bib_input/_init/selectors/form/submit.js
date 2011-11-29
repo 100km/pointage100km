4485d65187caf131311718b7496c2b4ca7f07b38
Jon Harper <jon.harper87@gmail.com>
2011-11-30 00:59:46 +0100

    Try to get results from a synchronous query, doesn't work

--- bib_input/evently/bib_input/_init/selectors/form/submit.js
+++ bib_input/evently/bib_input/_init/selectors/form/submit.js
@@ -3,6 +3,18 @@ function() {
   var fdoc = {};
   fdoc.bib  = form["bib"].value;
   fdoc.type = "contestant-checkpoint";
+
+  var res;
+  $$(this).app.db.view("bib_input/contestant-checkpoints", {
+                async : false,
+                success: function(data) {
+                   $.log("data is ");
+                   $.log(data);
+                   res = data;
+                }});
+  $.log("res is");
+  $.log(res);
+
   $$(this).app.db.saveDoc(fdoc, {
     success : function() {
       form.reset();
