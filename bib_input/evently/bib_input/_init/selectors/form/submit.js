2d936f027443ca732100856f8bb9f78b607c8274
Jon Harper <jon.harper87@gmail.com>
2011-12-01 00:11:18 +0100

    Add the current lap when inserting a checkpoint

--- bib_input/evently/bib_input/_init/selectors/form/submit.js
+++ bib_input/evently/bib_input/_init/selectors/form/submit.js
@@ -3,22 +3,21 @@ function() {
   var fdoc = {};
   fdoc.bib  = form["bib"].value;
   fdoc.type = "contestant-checkpoint";
+  var app = $$(this).app;
 
-  var res;
-  $$(this).app.db.view("bib_input/contestant-checkpoints", {
-                async : false,
+  app.db.view("bib_input/current-lap", {
+                key: fdoc.bib,
+                group: true,
                 success: function(data) {
                    $.log("data is ");
                    $.log(data);
-                   res = data;
-                }});
-  $.log("res is");
-  $.log(res);
-
-  $$(this).app.db.saveDoc(fdoc, {
-    success : function() {
-      form.reset();
-    }
-  });
+                   fdoc.lap = (data["rows"][0] && data["rows"][0]["value"] + 1) || 1;
+                   $.log("extracted is");
+                   $.log(fdoc.lap);
+                   app.db.saveDoc(fdoc, {
+                        success : function() {
+                        form.reset();
+                        }
+                   });}});
   return false;
 };
