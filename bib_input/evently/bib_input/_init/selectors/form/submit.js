fab48106b5d3a836a5119700cdc3ca823bc7a2cc
Jon Harper <jon.harper87@gmail.com>
2011-12-02 01:13:09 +0100

    BUGFIX: reduce possibility to add 2 entries with same lap;
    TODO, find a not-broken way to do this..

--- bib_input/evently/bib_input/_init/selectors/form/submit.js
+++ bib_input/evently/bib_input/_init/selectors/form/submit.js
@@ -1,7 +1,11 @@
 function() {
   var form = $(this)[0];
+  var bib = form["bib"].value;
+  if (bib == "") return false;
+  form.reset();
+
   var fdoc = {};
-  fdoc.bib  = form["bib"].value;
+  fdoc.bib  = bib;
   fdoc.type = "contestant-checkpoint";
   var app = $$(this).app;
 
@@ -14,10 +18,7 @@ function() {
                    fdoc.lap = (data["rows"][0] && data["rows"][0]["value"] + 1) || 1;
                    $.log("extracted is");
                    $.log(fdoc.lap);
-                   app.db.saveDoc(fdoc, {
-                        success : function() {
-                        form.reset();
-                        }
-                   });}});
+                   app.db.saveDoc(fdoc)
+		}});
   return false;
 };
