09a3f60bf53b246becdc7c8b68e43736965c2eb0
Tomô <thomas.gambier@gmail.com>
2011-12-10 01:15:15 +0100

    Cosmetic changes

--- bib_input/evently/items/_changes/data.js
+++ bib_input/evently/items/_changes/data.js
@@ -6,7 +6,9 @@ function(data) {
       p.bib = r.value && r.value.bib;
       p.lap = r.value && r.value.lap;
       p.ts  = r.key;
-      p.time = new Date(r.key);
+      date = new Date(r.key)
+      p.time_day = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear();
+      p.time_hour = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
       return p;
     })
   }
