507482b42208a682f640b46c9e6609e895266dd8
Tomô <thomas.gambier@gmail.com>
2011-12-12 23:18:43 +0100

    Split into a separated lib

---
+++ initialize_db/update_document.bash
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+
+which curl &> /dev/null || { echo "This script needs curl. Please install it." ; exit 1 ; }
+
+
+function update_document () {
+  url=$1
+  doc_json="$2"
+
+  get_request=$( curl -s -X GET $url )
+  if ! [[ "$get_request" =~ "missing" ]]
+  then
+    rev=$( echo $get_request | sed 's#.*_rev\":\"\([^\"]*\)\".*#\1#' )
+    echo "The document is already present with revision $rev. Need to add rev."
+data=$(cat <<EOF
+{
+"_rev":"$rev",
+$doc_json
+}
+EOF
+)
+  else
+data=$(cat <<EOF
+{
+$doc_json
+}
+EOF
+)
+  fi
+
+  curl -X PUT $url -H "Content-Type: application/json" --data "$data"
+}
+
+function post_document () {
+  url=$1
+  doc_json="$2"
+
+  data=$(cat <<EOF
+{
+$doc_json
+}
+EOF
+)
+
+  curl -X POST $url -H "Content-Type: application/json" --data "$data"
+}
