6d1a2bc22f7e56b186e125d807e563b8ff2b2a81
Thomas Gambier <thomas.gambier@gmail.com>
2011-12-14 14:14:13 +0100

    Display names in items

--- bib_input/evently/items/post_changes/async.js
+++ bib_input/evently/items/post_changes/async.js
@@ -9,7 +9,18 @@ function(cb) {
     startkey : [(site_id+1),0],
     endkey : [site_id,0],
     success: function(data) {
-      cb(data);
+      app.db.view("bib_input/bib_info", {
+        success: function(data_info) {
+          var map = {};
+          for (var i=0; i<data_info.rows.length; i++) {
+            map[data_info.rows[i].value.dossard] = {prenom:data_info.rows[i].value.prenom, nom:data_info.rows[i].value.nom, course:data_info.rows[i].value.course};
+          }
+          var new_data = data.rows.map(function(r) {
+            return {value:r.value, key:r.key, infos:map[r.value.bib]};
+          });
+          cb(new_data);
+        }
+      });
     }
   });
 };
