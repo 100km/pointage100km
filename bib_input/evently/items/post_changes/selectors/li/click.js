39c0577f47ad00604361750c7ed6c27ccd1e6cca
Tomo <thomas.gambier@gmail.com>
2011-12-18 23:33:24 +0100

    Change current_bib when a new inpuit is added. Still have a problem at the first loading of the page

--- bib_input/evently/items/post_changes/selectors/li/click.js
+++ bib_input/evently/items/post_changes/selectors/li/click.js
@@ -1,18 +1,8 @@
 function() {
   var app = $$(this).app;
-  var $_arrow=$("#arrow");
   var $_this=$(this);
-  var pos = $_this.offset();
-  var x_offset = $_this.width();
-  var y_offset = ($_this.height()-42)/2+4; // +4 because of the border
-  pos.top = pos.top + y_offset;
-  pos.left = pos.left + x_offset;
 
-  // Set the div to link concurrent with its infos
-  $_arrow.css({ position: "absolute",
-                marginLeft: 0, marginTop: 0,
-                top: pos.top, left: pos.left });
-  $_arrow.show();
+  place_arrow($_this);
 
   // First clear all lines
   $_this.parents("ul").children().children().css("font-weight", "");
@@ -24,4 +14,6 @@ function() {
   // Keep this and current bib into app
   app.current_li = $_this;
   app.current_bib = $_this.find("#delete")[0]["bib"]["value"];
+
+  $(this).trigger("change_infos");
 }
