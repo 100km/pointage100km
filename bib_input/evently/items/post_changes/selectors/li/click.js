82bb294bfbaada1aceacd24232ca6d8d9eab922d
Tomo <thomas.gambier@gmail.com>
2011-12-18 16:18:04 +0100

    Better colors and kee current bib inside the app

--- bib_input/evently/items/post_changes/selectors/li/click.js
+++ bib_input/evently/items/post_changes/selectors/li/click.js
@@ -1,4 +1,5 @@
 function() {
+  var app = $$(this).app;
   var $_arrow=$("#arrow");
   var $_this=$(this);
   var pos = $_this.offset();
@@ -6,16 +7,21 @@ function() {
   var y_offset = ($_this.height()-42)/2+4; // +4 because of the border
   pos.top = pos.top + y_offset;
   pos.left = pos.left + x_offset;
+
+  // Set the div to link concurrent with its infos
   $_arrow.css({ position: "absolute",
                 marginLeft: 0, marginTop: 0,
                 top: pos.top, left: pos.left });
+  $_arrow.show();
 
   // First clear all lines
-  $(this).parents("ul").children().children().css("font-weight", "");
-  $(this).parents("ul").children().css("background-color","white");
+  $_this.parents("ul").children().children().css("font-weight", "");
+  $_this.parents("ul").children().css("background-color","white");
   // Then set the clicked lines to bold
-  $(this).children().css("font-weight", "bold");
-  $(this).css("background-color", "#c0ffc0");
+  $_this.children().css("font-weight", "bold");
+  $_this.css("background-color", "#d0ffd0");
 
-  $_arrow.show();
+  // Keep this and current bib into app
+  app.current_li = $_this;
+  app.current_bib = $_this.find("#delete")[0]["bib"]["value"];
 }
