e61f84a6cffd50bcf485f6b9d58992a28552dd7f
Tomo <thomas.gambier@gmail.com>
2011-12-18 15:09:49 +0100

    Better way to set absolute position of the arrow

--- bib_input/evently/items/post_changes/selectors/li/click.js
+++ bib_input/evently/items/post_changes/selectors/li/click.js
@@ -3,9 +3,12 @@ function() {
   var $_this=$(this);
   var pos = $_this.offset();
   var x_offset = $_this.width();
-  var y_offset = $_this.height()/4;
+  var y_offset = ($_this.height()-30)/4;
+  $.log("In on click pos : " + JSON.stringify(pos) + "yoffset " + x_offset);
   pos.top = pos.top + y_offset;
   pos.left = pos.left + x_offset;
-  $_arrow.offset(pos);
+  $_arrow.css({ position: "absolute",
+                marginLeft: 0, marginTop: 0,
+                top: pos.top, left: pos.left });
   $_arrow.show();
 }
