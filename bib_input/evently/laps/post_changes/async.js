b969867417f1aac6a827be25480c3a5a4573ac93
Tomo <thomas.gambier@gmail.com>
2011-12-18 11:56:29 +0100

    BUGFIX: global ranking needs to sort by laps first whereas local ranking sort by site_id first in order to select only local site_id

--- bib_input/evently/laps/post_changes/async.js
+++ bib_input/evently/laps/post_changes/async.js
@@ -2,7 +2,7 @@ function(cb) {
   var app = $$(this).app;
   var site_id = app.site_id;
 
-  app.db.list("bib_input/global-ranking","rankings", {
+  app.db.list("bib_input/global-ranking","global-ranking", {
     limit : 50,
     success: function(data) {
       cb(data);
