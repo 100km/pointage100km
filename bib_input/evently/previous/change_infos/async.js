0013228b53d471af5afe296037b2cb60cbf6aa95
Tomô <thomas.gambier@gmail.com>
2011-12-25 08:17:00 +0100

    Use current_bib instead of last added bib in the right pane

--- bib_input/evently/previous/change_infos/async.js
+++ bib_input/evently/previous/change_infos/async.js
@@ -1,26 +1,15 @@
 function(cb) {
   var app = $$(this).app;
   var site_id = app.site_id;
+  var bib = app.current_bib;
 
-  app.db.view("bib_input/recent-checkpoints", {
-    limit: 1,
-    descending: true,
-    startkey : [(site_id+1),0],
-    endkey : [site_id,0],
+  app.db.list("bib_input/next-contestants", "local-ranking", {
+    startkey : [-site_id,null,null],
+    endkey : [-site_id+1,null,null],
+    bib: bib,
+    n: 3,
     success: function(data) {
-      if (data.rows.length > 0) {
-        var bib = data.rows[0]["value"]["bib"];
-        app.db.list("bib_input/next-contestants", "local-ranking", {
-          startkey : [-site_id,null,null],
-          endkey : [-site_id+1,null,null],
-          bib: bib,
-          n: 3,
-          success: function(data) {
-            cb(data);
-          }
-        });
-      } else
-        cb({});
+      cb(data);
     }
   });
 };
