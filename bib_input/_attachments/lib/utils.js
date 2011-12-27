60cf27ee129984316712e100ea036cd92d035e76
Tomô <thomas.gambier@gmail.com>
2011-12-27 06:58:41 +0100

    CLEANUP: indentation

--- bib_input/_attachments/lib/utils.js
+++ bib_input/_attachments/lib/utils.js
@@ -9,33 +9,33 @@ function call_with_checkpoints(bib, app, f) {
 }
 
 function new_checkpoints(bib, site_id) {
-        var checkpoints = {};
-        checkpoints.bib = bib;
-        checkpoints.site_id = site_id;
-        checkpoints.times = [];
-        return checkpoints;
+  var checkpoints = {};
+  checkpoints.bib = bib;
+  checkpoints.site_id = site_id;
+  checkpoints.times = [];
+  return checkpoints;
 }
 
 function add_checkpoint(checkpoints) {
-        var ts = new Date().getTime();
-        checkpoints["times"].push(ts);
+  var ts = new Date().getTime();
+  checkpoints["times"].push(ts);
 }
 
 function remove_checkpoint(checkpoints, ts) {
-	$.log("removing " + ts + " in " + checkpoints["times"]);
-	//Why doesn't indexOf work ?!?
-	for (var i = checkpoints["times"].length-1; i>=0; i--) {
-		if (ts == checkpoints["times"][i]) {
-			checkpoints["times"].splice(i, 1);
-			return;
-		}
-	}
+  $.log("removing " + ts + " in " + checkpoints["times"]);
+  //Why doesn't indexOf work ?!?
+  for (var i = checkpoints["times"].length-1; i>=0; i--) {
+    if (ts == checkpoints["times"][i]) {
+      checkpoints["times"].splice(i, 1);
+      return;
+    }
+  }
 }
 
 function isBib(bib)
 {
-    var isBib_re       = /^\d+$/;
-    return String(bib).search (isBib_re) != -1
+  var isBib_re       = /^\d+$/;
+  return String(bib).search (isBib_re) != -1
 }
 
 function place_arrow(obj) {
