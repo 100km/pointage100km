fa2244258e02c3ebedbc0ddaf37d46388f5eeffc
Tomo <thomas.gambier@gmail.com>
2012-01-07 15:31:19 +0100

    Let the info always at the same place in the browser in order to always see it even when scrolling. Also, the arrow is now dependent on the resize event. This solves Issue #5 and Issue #9

--- bib_input/_attachments/lib/utils.js
+++ bib_input/_attachments/lib/utils.js
@@ -52,7 +52,9 @@ function isBib(bib)
 }
 
 function place_arrow(obj) {
-  var app = $$(this).app;
+  if (obj == undefined)
+    return;
+
   var $_arrow=$("#arrow");
   var pos = obj.offset();
 
@@ -62,11 +64,10 @@ function place_arrow(obj) {
   var x_offset = obj.width();
   var y_offset = (obj.height()-42)/2+4; // +4 because of the border
   pos.top = pos.top + y_offset;
-  pos.left = pos.left + x_offset;
+  pos.left = pos.left + x_offset - 100; // should not be necessary but prevent some artifact
 
   // Set the div to link concurrent with its infos
-  $_arrow.css({ position: "absolute",
-                marginLeft: 0, marginTop: 0,
+  $_arrow.css({ marginLeft: 0, marginTop: 0,
                 top: pos.top, left: pos.left });
   $_arrow.show();
 }
