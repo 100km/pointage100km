07b39102322fb0629dd415f6504ba4c6e70afdb7
Samuel Tardieu <sam@rfc1149.net>
2012-01-15 23:25:56 +0100

    Replicate database, wait for end, and compact.

--- couchsync/src/main/scala/Config.scala
+++ couchsync/src/main/scala/Config.scala
@@ -1,13 +1,41 @@
+import dispatch._
 import java.io.{File, FileWriter}
 import java.lang.{Process, ProcessBuilder}
+import net.liftweb.json._
+import net.rfc1149.canape._
 import scala.io.Source
 
 object Config {
 
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
     val c = new Config(new File(args(0)), new File(args(1)))
-    c.fixDefaultIni()
     c.runCouchDb()
+    Thread.sleep(5000)
+    val couch = c.couch
+    val db = couch.db("steenwerck100km")
+    try {
+      Http(db.create())
+    } catch {
+	case _ =>
+    }
+    val referenceDb = Couch(args(2), "admin", "admin").db("steenwerck100km")
+    Http(couch.replicate(db, referenceDb, false))
+    Http(couch.replicate(referenceDb, db, false))
+
+    var activeTasksCount: Int = -2
+    do {
+      activeTasksCount = Http(couch.activeTasks).children.size
+      println("Active tasks count: " + activeTasksCount)
+      Thread.sleep(100)
+    } while (activeTasksCount > 0)
+
+    db.startCompaction
+    while (Http(db.status).compact_running) {
+      println("Compaction running")
+      Thread.sleep(100)
+    }
+
+    c.stopCouchDb()
   }
 
 }
@@ -18,15 +46,18 @@ class Config(srcDir: File, usbBaseDir: File) {
   val etcDir = new File(usbBaseDir, "etc")
   val runDir = new File(usbBaseDir, "run")
 
-  dbDir.mkdirs()
-  etcDir.mkdirs()
-  runDir.mkdirs()
+  private[this] var process: Option[Process] = None
 
   val defaultFile = new File(etcDir, "default.ini")
-  val localFile = new File(srcDir, "local.ini")
   val logFile = new File(runDir, "couchdb.log")
   val pidFile = new File(runDir, "couchdb.pid")
   val uriFile = new File(runDir, "couchdb.uri")
+
+  private[this] var _couch: Option[Couch] = None
+
+  dbDir.mkdirs()
+  etcDir.mkdirs()
+  runDir.mkdirs()
 
   def fixDefaultIni() = {
     val ini = new Ini
@@ -48,11 +79,24 @@ class Config(srcDir: File, usbBaseDir: File) {
     out.close()
   }
 
-  def runCouchDb(): Process = {
+  def couch = _couch.get
+
+  def runCouchDb() = {
+    fixDefaultIni()
+
     val pb = new ProcessBuilder("/usr/bin/couchdb",
 				"-n", "-a", defaultFile.toString,
 				"-p", pidFile.toString)
     pb.directory(usbBaseDir)
-    pb.start()
+    process = Some(pb.start())
+
+    _couch = Some(Couch("localhost", 5983, "admin", "admin"))
   }
+
+  def stopCouchDb() = {
+    process foreach (_.destroy())
+    process = None
+    _couch = None
+  }
+
 }
