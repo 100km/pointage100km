8716388c2c47ebfc7b6615fc9d44070ae7ee9a98
Tomo <thomas.gambier@gmail.com>
2012-01-19 23:32:56 +0100

    Remove names in local document. They are now in a shared document.
    Also rename site_id in site-id in the JSON.

--- bib_input/evently/title/_changes/data.js
+++ bib_input/evently/title/_changes/data.js
@@ -1,7 +1,4 @@
 function(data) {
-    // Set title for the document
-    document.title = "Pointage " + data.name;
-
     // Set field in app so that everyone can access the site_id
     var app = $$(this).app;
     app.site_id = data.site_id
@@ -9,8 +6,11 @@ function(data) {
     app.sites_nb = data.sites.length
     $(this).trigger("post_changes");
 
+    // Set title for the document
+    document.title = "Pointage " + app.sites[app.site_id];
+
     return {
-        site_name : data.name,
+        site_name : app.sites[app.site_id],
         site_id : data.site_id
     }
 };
