0ca4561ffa27eec1db3bf0214e68a881a7a9fbb6
Samuel Tardieu <sam@rfc1149.net>
2012-01-19 10:12:09 +0100

    Simplify implicit interface

--- canape/src/main/scala/Result.scala
+++ canape/src/main/scala/Result.scala
@@ -4,17 +4,21 @@ import dispatch._
 import dispatch.liftjson.Js._
 import net.liftweb.json._
 
-class Result[K, V](js: JValue, implicit val formats: Formats)(implicit val k: Manifest[K], implicit val v: Manifest[V]) {
+final class Result[K: Manifest, V: Manifest](js: JValue,
+					     formats: Formats) {
 
   val JInt(total_rows: BigInt) = js \ "total_rows"
   val JInt(offset: BigInt) = js \ "offset"
-  val rows: List[Result.Row[K, V]] = (js \ "rows").children map { new Result.Row[K, V](_) }
+  val rows: List[Result.Row[K, V]] = (js \ "rows").children map { new Result.Row[K, V](_, formats) }
 
 }
 
 object Result {
 
-  class Row[K, V](js: JValue)(implicit val formats: Formats, implicit val k: Manifest[K], implicit val v: Manifest[V]) {
+  final class Row[K: Manifest, V: Manifest](js: JValue,
+					    formats: Formats) {
+
+    private[this] implicit val f = formats
 
     val JString(id: String) = js \ "id"
     val key: K = (js \ "key").extract[K]
