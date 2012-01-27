71d13380734bdcb85d6f06a977a847695af7f9ca
Tomo <thomas.gambier@gmail.com>
2012-01-27 22:45:45 +0100

    Better layout for previous info. Solves issue #11.

--- bib_input/evently/previous/change_infos/data.js
+++ bib_input/evently/previous/change_infos/data.js
@@ -3,11 +3,8 @@ function(data) {
 
   data.current_bib = app.current_bib;
   data.current_lap = app.current_lap;
-  data.nom = data.infos.nom
-  data.prenom = data.infos.prenom
-  data.adresse = data.infos.adresse;
-  data.cp = data.infos.cp;
-  data.commune = data.infos.commune;
+  data.nom = data.infos.nom;
+  data.prenom = data.infos.prenom;
 
   return data;
 };
