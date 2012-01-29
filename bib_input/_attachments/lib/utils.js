55d11641590f066e3958f9f47d7caefe83ec8a15
Jon Harper <jon.harper87@gmail.com>
2012-01-29 15:04:36 +0100

    Implement new message structure: see issue #22

--- bib_input/_attachments/lib/utils.js
+++ bib_input/_attachments/lib/utils.js
@@ -259,14 +259,48 @@ function normalize_message_id(str) {
     tmp.pop();
   return tmp.join("-");
 }
-function call_with_messages(app, cb) {
-  var id = app.site_id;
-  app.db.allDocs({
-    include_docs: true,
-    keys:[local_messages_id(id), admin_messages_id(id), admin_broadcast_id()],
-    success: cb
+
+function unwrap_messages(data) {
+  return data.rows.map(function(row) {
+    return row.value;
   });
 }
+function _call_with_messages(app, startkey, endkey, cb) {
+  app.db.view("bib_input/all-messages", {
+    startkey: startkey,
+    endkey:   endkey,
+    success: function(data) {
+    cb(unwrap_messages(data));
+    }
+  });
+}
+function call_with_site_messages(app, cb) {
+  var id = app.site_id;
+  _call_with_messages(app, [id,true], [id+1], cb);
+}
+function call_with_bcast_messages(app, cb) {
+  _call_with_messages(app, [null,true], [false], cb);
+}
+function call_with_local_status(app, cb) {
+  app.db.openDoc("_local/status", {
+    success: cb,
+    error: function(a,b,c) { cb(""); }
+  });
+}
+function call_with_messages(app, cb) {
+  fork([
+    function(cb) {call_with_site_messages(app, cb)},
+    function(cb) {call_with_bcast_messages(app, cb)},
+    function(cb) {call_with_local_status(app, cb)}
+  ], function(data) {
+    var res = {};
+    res.site_messages = data[0][0];
+    res.bcast_messages = data[1][0];
+    res.local_status = data[2][0];
+    cb(res);
+  });
+}
+
 
 function checkBib(bib) {
   if (bib == "") {
