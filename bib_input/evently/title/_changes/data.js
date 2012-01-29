0babf772f7371a7d8df4463bdcbce3c8f5a59724
Tomo <thomas.gambier@gmail.com>
2012-01-29 13:09:47 +0100

    Display absolute hour along the race time. Fixes issue #17.

--- bib_input/evently/title/_changes/data.js
+++ bib_input/evently/title/_changes/data.js
@@ -5,6 +5,8 @@ function(data) {
     app.sites = data.infos["sites"]
     app.sites_nb = app.sites.length
     app.races_names = data.infos["races_names"]
+    app.kms_site = [data.infos["kms_site0"], data.infos["kms_site1"], data.infos["kms_site2"]]
+    app.start_times = data.infos["start_times"]
     $(this).trigger("post_changes");
 
     // Set title for the document
