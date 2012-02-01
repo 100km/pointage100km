bdb3504681f7c430fa52a3d607c8918d6365aab3
sebastian <seba@mymail.com>
2012-01-31 20:31:02 -0500

    update, although handling a form with two fields is still not working

--- bib_input/evently/bib_admin_input_message_div/_init/selectors/form/keyup.js
+++ bib_input/evently/bib_admin_input_message_div/_init/selectors/form/keyup.js
@@ -1,17 +1,25 @@
 function(ev) {
-  key = ev.which?ev.which:window.event.keyCode;
+    key = ev.which ? ev.which : window.event.keyCode;
 
-  bib_form = $("#bib_input").find("input")[0];
-  bib_value = bib_form.value;
+    bib_admin_message_form = $("#bib_admin_input_message_div").find("input")[0];
+    admin_message = bib_admin_message_form.value;
+
+    bib_admin_message_target_form = $("#bib_admin_input_message_div").find("input")[1];
+    admin_message_target = bib_admin_message_target_form.value;
 
   // In Chrome, up and down array change the cursor location, always set it to the end
-  if ((key == 38) || (key == 40)) {
-    bib_form.selectionStart = bib_value.length;
-    bib_form.selectionEnd = bib_value.length;
-  }
+    if ((key == 38) || (key == 40)) {
+	bib_admin_message_form.selectionStart = admin_message.length;
+	bib_admin_message_form.selectionEnd   = admin_message.length;
+	bib_admin_message_target_form.selectionStart = admin_message_target.length;
+	bib_admin_message_target_form.selectionEnd   = admin_message_target.length;
+    }
+    
+    $("#message_len").html("len=" + admin_message.length + "/128");
+    
 
-  checkBib(bib_value);
-
-  // let the event continue
-  return true;
+    //checkBib(bib_value);
+    
+    // let the event continue
+    return true;
 };
--- bib_input/evently/bib_admin_input_message_div/_init/selectors/form/submit.js
+++ bib_input/evently/bib_admin_input_message_div/_init/selectors/form/submit.js
@@ -1,14 +1,19 @@
 function() {
-  var form = $(this)[0];
-  var bib = form["bib"].value; // we are sure it's an integer because of the regexp check.
-  if (! isBib(bib)) return false;
-  bib = parseInt(bib, 10);
-  form.reset();
+    var form = $(this)[0];
+    var message = form["bib_admin_message"].value; 
+    var message_target = form["bib_admin_message_target"].value;
 
-  var app = $$(this).app;
-  app.current_bib = 0;
-  app.current_lap = 0;
+    $.log("message " + message + " target " + message_target);
 
-  submit_bib(bib, app);
-  return false;
+// we are sure it's an integer because of the regexp check.
+  //if (! isBib(bib)) return false;
+  //bib = parseInt(bib, 10);
+    form.reset();
+
+  //var app = $$(this).app;
+  //app.current_bib = 0;
+  //app.current_lap = 0;
+
+//  submit_bib(bib, app);
+    return false;
 };
