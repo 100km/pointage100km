fc50637d7b9896cef00251c4908d934eb2dc49cc
Tomo <thomas.gambier@gmail.com>
2012-02-02 21:57:46 +0100

    Display the local average. Issue #25.

---
+++ bib_input/views/times-per-bib/map.js
@@ -0,0 +1,7 @@
+// View : times-per-bib
+// Show all the times for each bib. Let us know the last known checkpoint (for the average).
+function(doc) {
+  for (var lap=0; lap<(doc.times && doc.times.length) || 0; lap++) {
+    emit([doc.bib, doc.times[lap]], [doc.site_id, lap+1]);
+  }
+};
