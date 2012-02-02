100km/pointage100km

commit c681a8d5687cc84f871d0d82326f2b0d356b3292

Load only needed contestant-infos. fixes issue #34

bib_input/evently/items/post_changes/async.js
@@ -9,16 +9,25 @@ function(cb) {
     startkey : [(site_id+1),0],
     endkey : [site_id,0],
     success: function(data) {
-      app.db.view("bib_input/bib_info", {
-        success: function(data_info) {
-          var map = {};
-          for (var i=0; i<data_info.rows.length; i++) {
-            map[data_info.rows[i].value.dossard] = {prenom:data_info.rows[i].value.prenom, nom:data_info.rows[i].value.nom, course:data_info.rows[i].value.course};
-          }
-          var new_data = data.rows.map(function(r) {
-            return {value:r.value, key:r.key, infos:map[r.value.bib]};
+      var allDocs_keys=_.uniq(data.rows.map(function(row) {
+        return infos_id(row.value.bib);
+      }));
+      app.db.allDocs({
+        keys: allDocs_keys,
+        include_docs: true,
+        success: function(infos) {
+          var hash = {};
+          _.each(infos.rows, function(row) {
+            hash[row.doc.dossard] = {
+              prenom: row.doc.prenom,
+              nom: row.doc.nom,
+              course: row.doc.course
+              };
           });
-          cb(new_data);
+          _.each(data.rows, function(row) {
+            row.infos = hash[row.value.bib];
+          });
+          cb(data.rows);
         }
       });
     }

bib_input/_attachments/pointage.html
@@ -15,6 +15,7 @@
 
   </body>
   <script src="vendor/couchapp/loader.js"></script>
+  <script src="lib/underscore-min.js"></script>
   <script src="lib/utils.js"></script>
   <script type="text/javascript" charset="utf-8">
     $(document).ready(function(){

commit 6d40c114b4a4185abafee0e4b4b5014508e11794

BUGFIX: fix bibs without infos (broken since c681a8d)

bib_input/evently/items/post_changes/async.js
@@ -18,11 +18,13 @@ function(cb) {
         success: function(infos) {
           var hash = {};
           _.each(infos.rows, function(row) {
-            hash[row.doc.dossard] = {
-              prenom: row.doc.prenom,
-              nom: row.doc.nom,
-              course: row.doc.course
-              };
+            if (row.doc) {
+              hash[row.doc.dossard] = {
+                prenom: row.doc.prenom,
+                nom: row.doc.nom,
+                course: row.doc.course
+                };
+            }
           });
           _.each(data.rows, function(row) {
             row.infos = hash[row.value.bib];