b6511dcefd2eb08cabad25ee22c343bd1d59829a
sebastian <seba@mymail.com>
2012-02-06 16:43:18 -0500

    add specific view for the messages

---
+++ bib_input/views/all-valid-messages/map.js
@@ -0,0 +1,8 @@
+// View : all-valid-messages
+// Used to display the messages in bib_admin_message_list_div
+function(doc) {
+  if (doc.message) {
+    var active = doc.deletedTS == undefined;
+    emit([active, doc.addedTS], doc);
+  }
+};
