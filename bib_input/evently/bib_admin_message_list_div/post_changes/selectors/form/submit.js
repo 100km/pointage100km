bec2ba3784c71ad61bd6a344f86893e0ef24921f
sebastian <seba@mymail.com>
2012-02-11 18:16:41 -0500

    delete a message working

--- bib_input/evently/bib_admin_message_list_div/post_changes/selectors/form/submit.js
+++ bib_input/evently/bib_admin_message_list_div/post_changes/selectors/form/submit.js
@@ -1,22 +1,54 @@
 function() {
-  var form = $(this)[0];
-  var bib = form["bib"].value;
-  var lap = form["lap"].value;
-  var ts = form["ts"].value;
-  if (bib == "" || ts == "" || lap == "" ) return false;
-  bib = parseInt(bib);
-  lap = parseInt(lap);
+    var app = $$(this).app;
+    var form = $(this)[0];
+    var message_to_modify = {};
+    var message_to_delete = {};
+    var message_id     = form["id"].value;
+    var message_rev    = form["rev"].value;
+    var message_target = form["target"].value;
+    var message_body   = form["body"].value;
+    var message_addedTS = form["addedTS"].value;
 
-  var app = $$(this).app;
-  if ((bib == app.current_bib) && (lap == app.current_lap)) {
-    // $.log("ERASING current_li");
-    app.current_li = null;
-    app.current_bib = 0;
-    app.current_lap = 0;
-  }
+    form.reset();
 
-  $(this).parents("li").hide('fast', function() {
-      submit_remove_checkpoint(bib, app, ts);
-  });
-  return false;
+    $.log("clicked on message: id='" + message_id + "', body='" + message_body + "' for target=" + message_target);
+
+    message_to_modify._id       = message_id;
+    message_to_modify.message   = message_body;
+    message_to_modify.target    = message_target;
+    message_to_modify.addedTS   = message_addedTS;
+    message_to_modify.deletedTS = new Date().getTime();
+
+    message_to_delete._id       = message_id;
+    message_to_delete._rev      = message_rev;
+
+    function callback_error(stat, err, reason) {
+	$.log("stat" + JSON.stringify(stat));
+	$.log("err" + JSON.stringify(err));
+	$.log("reason" + JSON.stringify(reason));
+	alert("Error adding message to db: " + JSON.stringify(reason));
+    }
+
+    function callback_ok() {
+	$.log("ok");
+    }
+
+
+    function mark_message_as_deleted() {
+	$.log("deleting");
+
+	//not working, using "removeDoc" instead
+	app.db.saveDoc(message_to_modify, { success: callback_ok, error: callback_error });
+    }
+
+    function delete_message() {
+	$.log("deleting");
+
+	app.db.removeDoc(message_to_delete, { success: callback_ok, error: callback_error });
+    }
+
+    $(this).parents("li").hide('fast', delete_message());
+
+    return false;
+
 };
--- bib_input/evently/bib_admin_message_list_div/post_changes/selectors/li/click.js
+++
@@ -1,6 +0,0 @@
-function() {
-  var app = $$(this).app;
-  var $_this=$(this);
-
-  change_li($_this, app);
-}
