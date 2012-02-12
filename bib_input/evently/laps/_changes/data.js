cbc0b70fed7806590cce1a58f0edffc8f6caf03e
sebastian <seba@mymail.com>
2012-02-12 12:47:05 -0500

    better, moved twitter to evently's data.js

--- bib_input/evently/laps/_changes/data.js
+++ bib_input/evently/laps/_changes/data.js
@@ -1,16 +1,37 @@
 function(data) {
-  var p;
-  return {
-    races: data.rows.map(function(pair) {
-      return {
-        race_id: pair.race_id,
-        items: pair.contestants.map(function(r) {
-          p = {};
-          p.bib = r.value && r.value.bib;
-          p.lap = r.value && r.value.times.length;
-          return p;
-        })
-      }
-    })
-  }
+    var p;
+
+    //sample code from: http://webhole.net/2009/11/28/how-to-read-json-with-javascript/ 
+    var url='http://search.twitter.com/search.json?callback=?&q=';
+    var query;
+
+    function query_twitter() {
+	query = "#paris"; //$("#query").val();
+	query = escape(query);
+	
+	$.log("querying twitter with: " + url + query);
+	
+	$.getJSON(url+query, function(json) {
+	    $.each(json.results, function(i, tweet) {
+		$("#results").append('<p><img src="'+tweet.profile_image_url+'" widt="48" height="48" />'+tweet.text+'</p>');
+	    });
+	});
+    }
+
+    query_twitter();
+    
+    
+    return {
+	races: data.rows.map(function(pair) {
+	    return {
+		race_id: pair.race_id,
+		items: pair.contestants.map(function(r) {
+		    p = {};
+		    p.bib = r.value && r.value.bib;
+		    p.lap = r.value && r.value.times.length;
+		    return p;
+		})
+	    }
+	})
+    }
 };
