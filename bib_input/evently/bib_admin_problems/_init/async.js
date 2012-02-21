542d390b1efad1ea0a5c410d4d32b195c51105a7
Tomo <thomas.gambier@gmail.com>
2012-02-21 23:25:30 +0100

    Move check_times function to utils.js and give it the last ping for each site. The pings aren't taken into account for now.

--- bib_input/evently/bib_admin_problems/_init/async.js
+++ bib_input/evently/bib_admin_problems/_init/async.js
@@ -1,8 +1,15 @@
 function(cb) {
   var app = $$(this).app;
 
-  app.db.view("bib_input/bib-problems", {
-    success: cb
-  });
+  fork([
+    function(cb) { get_doc(app, cb, "ping-site0") },
+    function(cb) { get_doc(app, cb, "ping-site1") },
+    function(cb) { get_doc(app, cb, "ping-site2") },
+    function(cb) {
+      app.db.view("bib_input/bib-problems", {
+        success: cb
+      });
+    }
+  ], cb);
 };
 
