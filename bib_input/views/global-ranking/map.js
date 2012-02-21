9ac8c2238af5ead100519fba4a4b79e2d2586a8b
Jon Harper <jon.harper87@gmail.com>
2012-02-21 18:07:25 +0100

    BUGFIX: fix bib 0 in global ranking

--- bib_input/views/global-ranking/map.js
+++ bib_input/views/global-ranking/map.js
@@ -1,7 +1,7 @@
 // View : global-ranking
 // Used for global ranking of all contestants (to be diplayed in the web site for example)
 function(doc) {
-  if (doc.bib && doc.times && doc.times.length > 0 && doc.site_id != undefined) {
+  if (doc.bib != undefined && doc.times && doc.times.length > 0 && doc.site_id != undefined) {
     var len = doc.times.length;
       emit([doc.race_id, -len, -doc.site_id, doc.times[len-1]], doc);
   }
