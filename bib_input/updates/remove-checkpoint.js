2a24ffb8acbd16d0ebda7427d774fb61398b3a25
Jon Harper <jon.harper87@gmail.com>
2012-02-25 23:16:08 +0100

    FEATURE: use update handler to submit and remove checkpoints. fixes #38

---
+++ bib_input/updates/remove-checkpoint.js
@@ -0,0 +1,22 @@
+function(doc, req) {
+  var ts = Number(req.form.ts);
+
+  var deleted = false;
+  doc.deleted_times = doc.deleted_times || [];
+  for (var i = doc.times.length-1; i>=0; i--) {
+    if (ts == doc.times[i]) {
+      //TODO maybe need to optimized sorted array insertion?
+      doc.deleted_times.push(doc.times.splice(i, 1)[0]);
+      doc.deleted_times.sort(function(a,b) {return a-b});
+      deleted = true;
+      break;
+    }
+  }
+
+  return [doc, {
+    headers : {
+     "Content-Type" : "application/json"
+     },
+    body: JSON.stringify(deleted)
+  }];
+};
