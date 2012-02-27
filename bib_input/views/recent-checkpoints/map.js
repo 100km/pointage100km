83563910543ba09d7556504fb27c2d5fd1f33bee
Tomo <thomas.gambier@gmail.com>
2012-02-28 00:13:41 +0100

    BUGFIX: laps start at 1

--- bib_input/views/recent-checkpoints/map.js
+++ bib_input/views/recent-checkpoints/map.js
@@ -5,7 +5,7 @@ function(doc) {
     for (var lap=0; lap<(doc.times && doc.times.length) || 0; lap++) {
       emit([doc.site_id, doc.times[lap]], {
         _id : "contestant-" + doc.bib,
-        lap: lap,
+        lap: lap+1,
         bib: doc.bib
       });
     }
