c166170624f30e6b218aec24d8a83a7281702091
Jon Harper <jon.harper87@gmail.com>
2012-02-27 22:41:50 +0100

    use linked document to avoid multile request to get associated document of the view recent_checkpoints

--- bib_input/evently/items/post_changes/async.js
+++ bib_input/evently/items/post_changes/async.js
@@ -2,36 +2,25 @@ function(cb) {
   var app = $$(this).app;
   var site_id = app.site_id;
 
-  // startkey and endkey are invrsed because descending is true
+  // startkey and endkey are inversed because descending is true
   app.db.view("bib_input/recent-checkpoints", {
     descending: true,
-    limit : 50,
+    include_docs: true,
+    limit : 30,
     startkey : [(site_id+1),0],
     endkey : [site_id,0],
     success: function(data) {
-      var allDocs_keys=_.uniq(data.rows.map(function(row) {
-        return infos_id(row.value.bib);
-      }));
-      app.db.allDocs({
-        keys: allDocs_keys,
-        include_docs: true,
-        success: function(infos) {
-          var hash = {};
-          _.each(infos.rows, function(row) {
-            if (row.doc) {
-              hash[row.doc.dossard] = {
-                prenom: row.doc.prenom,
-                nom: row.doc.nom,
-                course: row.doc.course
-                };
-            }
-          });
-          _.each(data.rows, function(row) {
-            row.infos = hash[row.value.bib];
-          });
-          cb(data.rows);
+      _.each(data.rows, function(row) {
+        if (row.doc) {
+          row.infos = {
+            prenom: row.doc.prenom,
+            nom: row.doc.nom,
+            course: row.doc.course
+          };
         }
       });
+
+      cb(data.rows);
     }
   });
 };
