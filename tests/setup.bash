8e022d48e4f0cb9db85405ad6d7bc546e9a38c55
Jon Harper <jon.harper87@gmail.com>
2012-03-02 00:55:16 +0100

    CLEANUP: first attempt at splitting couchapps
    I could not get couchapp pushapps to work correctly,
    so we have to use the small bash script pushapp.bash.
    
    I could not get the lib directory to just be a symlink
    of a directory in common (couchapp wouldn't push it), so
    it is a copy. Same for vendor, style and index.html.
    
    Note that this means that any modification to the copies
    will be lost, only edit files in common !

--- tests/setup.bash
+++ tests/setup.bash
@@ -4,9 +4,9 @@ which curl &> /dev/null || { echo "This script needs curl. Please install it." ;
 tested_couchapp="bib_input"
 
 rm -rf "./$tested_couchapp"
-cp -r "../$tested_couchapp" .
+cp -r "../couchapps/$tested_couchapp" .
+cp -r ../couchapps/common/* $tested_couchapp
 cp -r "overwrite/$tested_couchapp" .
-
 
 url="http://admin:admin@localhost:5984"
 curl -s -X DELETE "$url/test_db/" -H "Content-Type: application/json" 
