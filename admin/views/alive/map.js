f416cff3c1a16ad58977936049baa23984dec641
Samuel Tardieu <sam@rfc1149.net>
2012-03-01 21:57:18 +0100

    Fix typo

--- admin/views/alive/map.js
+++ admin/views/alive/map.js
@@ -1,7 +1,7 @@
 function(doc) {
     if (doc.site_id != undefined) {
 	if (doc.time)
-	    emit(doc.site_id, time);
+	    emit(doc.site_id, doc.time);
 	else if (doc.times && doc.times.length > 0)
 	    emit(doc.site_id, doc.times[doc.times.length - 1]);
     }
