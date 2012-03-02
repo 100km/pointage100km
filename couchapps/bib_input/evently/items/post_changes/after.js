f02a3554b07f7365d872922efd15152d7ff7aa96
Baptiste Pernet <bapt@sp4ce.net>
2012-03-02 17:56:20 +0100

    Remove useless click handler on form#delete - use event to access between widgets.

--- couchapps/bib_input/evently/items/post_changes/after.js
+++ couchapps/bib_input/evently/items/post_changes/after.js
@@ -5,15 +5,12 @@ function () {
   var selected_item = $(this).data('selected_item');
 
   // There is not selected item in the data of the widget, select the first one.
-  // TODO(bapt): consider to move this part in a upper part of the application, this logic should no be here.
-  // because it concerns when the user first come to the page.
   if (!selected_item) {
     var first_line = $(this).find('li:eq(1)');
     var bib = first_line.find('input[name="bib"]').val();
     var lap = first_line.find('input[name="lap"]').val();
     if (bib && lap) {
       selected_item = { bib : bib, lap: lap };
-      $(this).data('selected_item', selected_item);
     }
   }
 
