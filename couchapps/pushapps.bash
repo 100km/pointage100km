46f5a928960f267649f99c06f49986385ec93736
Baptiste Pernet <bapt@sp4ce.net>
2012-03-02 11:04:52 +0100

    The -l option is deprecated since the POSIX standard specifies -L instead.

--- couchapps/pushapps.bash
+++ couchapps/pushapps.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-couchapps=$(ls -1 */.couchapprc | xargs -l1 dirname)
+couchapps=$(ls -1 */.couchapprc | xargs -L1 dirname)
 for couchapp in $couchapps; do
   cp -rf common/* $couchapp/
   (cd $couchapp && couchapp push)
