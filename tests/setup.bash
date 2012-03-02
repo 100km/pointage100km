4b2632cebbd815160ac64a4880b6911d379fe707
Jon Harper <jon.harper87@gmail.com>
2012-03-02 21:30:21 +0100

    CLEANUP: Serve evently and static content from the 'common' couchapp

--- tests/setup.bash
+++ tests/setup.bash
@@ -1,16 +1,17 @@
 
 which curl &> /dev/null || { echo "This script needs curl. Please install it." ; exit 1 ; }
 
-tested_couchapp="bib_input"
-
-rm -rf "./$tested_couchapp"
-cp -r "../couchapps/$tested_couchapp" .
-cp -r ../couchapps/common/* $tested_couchapp
-cp -r "overwrite/$tested_couchapp" .
-
 url="http://admin:admin@localhost:5984"
-curl -s -X DELETE "$url/test_db/" -H "Content-Type: application/json" 
+test_db="$url/test_db"
+echo "Deleting previous test database..."
+curl -s -X DELETE "$test_db" -H "Content-Type: application/json"
 
-cd "./$tested_couchapp" && couchapp push
+couchapps_dir="../couchapps"
+needed_couchapps="common bib_input"
+for couchapp in $needed_couchapps; do
+  echo "Pushing needed couchapp $couchapp"
+  (cd $couchapps_dir/$couchapp && couchapp push $test_db)
+done
 
-
+echo "Pushing test couchapp. Launch the tests at the following URL"
+(cd "./test_couchapp" && couchapp push)
