8ecfa976fec6b7af5f8a5a1f0e36f44879566eee
Baptiste Pernet <bapt@sp4ce.net>
2012-03-02 18:35:15 +0100

    Clear the info box after the selected line is deleted

--- couchapps/bib_input/evently/items/post_changes/selectors/form/submit.js
+++ couchapps/bib_input/evently/items/post_changes/selectors/form/submit.js
@@ -5,14 +5,21 @@ function(e) {
   var bib = form.bib.value;
   var lap = form.bib.value;
   var ts = form.ts.value;
-  if (bib == "" || ts == "" || lap == "" ) return false;
+  if (bib == '' || ts == '' || lap == '') {
+    return false;
+  }
+
   bib = parseInt(bib);
   lap = parseInt(lap);
 
   // Hide the line and then delete with async query.
   var app = $$(this).app;
-  $(this).parents("li").hide('fast', function() {
-      submit_remove_checkpoint(bib, app, ts);
+  $(this).parents('li').hide('fast', function() {
+    if ($(this).hasClass('selected')) {
+      $(this).trigger('set_selected_item', null);
+      $(this).trigger('clear_infos');
+    }
+    submit_remove_checkpoint(bib, app, ts);
   });
 
   return false;
