c2533993912f3f01efb70ac11187bca1626d69dd
Baptiste Pernet <bapt@sp4ce.net>
2012-03-03 19:09:25 +0100

    Issue #56: store information on each item line to avoid a request to the DB.

--- couchapps/bib_input/evently/items/post_changes/after.js
+++ couchapps/bib_input/evently/items/post_changes/after.js
@@ -1,15 +1,26 @@
-function () {
+function (datas) {
   // Get the selected_item from the data of the widget.
   // This is set by the call make of the bib_input submit because we don't
   // a direct callback chain from the input to here, because this is the data base that trigger this event.
   var selected_item = $(this).data('selected_item');
 
+  $.each(datas, function(index, info) {
+    // Build the data object for the checkpoint.
+    var data = info.doc;
+    data.bib = info.value.bib;
+    data.lap = info.value.lap;
+    // Store information of each contestant, to avoid to retrieve them again from DB when you click on the element.
+    $('#js_checkpoint_' + info.value.bib + '_' + info.value.lap).data('checkpoint', data);
+    // If the selected item is matching, update with more data about the checkpoint.
+    if (selected_item && selected_item.bib == data.bib && selected_item.lap == data.lap) {
+      selected_item = data
+    }
+  });
+
   // There is not selected item in the data of the widget, select the first one.
   if (!selected_item) {
-    var first_line = $(this).find('li:eq(1)');
-    var bib = parseInt(first_line.find('input[name="bib"]').val());
-    var lap = parseInt(first_line.find('input[name="lap"]').val());
-    selected_item = { bib : bib, lap: lap };
+    var li = $(this).find('li:eq(1)');
+    selected_item = li.data('checkpoint');
   }
 
   // Trigger 'select_item' with the selected_item
--- couchapps/bib_input/evently/items/post_changes/selectors/li/click.js
+++ couchapps/bib_input/evently/items/post_changes/selectors/li/click.js
@@ -1,6 +1,3 @@
 function() {
-  var bib = parseInt($(this).find('input[name="bib"]').val());
-  var lap = parseInt($(this).find('input[name="lap"]').val());
-
-  $(this).trigger('select_item', { bib: bib, lap: lap });
+  $(this).trigger('select_item', $(this).data('checkpoint'));
 }
