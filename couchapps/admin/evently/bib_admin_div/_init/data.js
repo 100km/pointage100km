543dd9d1178bd564c896b669f472e9186e1e3a90
Tomo <thomas.gambier@gmail.com>
2012-03-05 19:32:17 +0100

    Use copy_app_data in administration couchapp

--- couchapps/admin/evently/bib_admin_div/_init/data.js
+++ couchapps/admin/evently/bib_admin_div/_init/data.js
@@ -1,12 +1,9 @@
 function(infos) {
     // Set field in app so that everyone can access the site_id
     var app = $$(this).app;
-    app.sites = infos["sites"]
-    app.sites_nb = app.sites.length
-    app.races_names = infos["races_names"]
-    app.kms_offset = [infos["kms_offset_site0"], infos["kms_offset_site1"], infos["kms_offset_site2"]]
-    app.kms_lap = infos["kms_lap"]
-    app.start_times = infos["start_times"]
+
+    copy_app_data(app, infos);
+
     $(this).trigger("post_changes");
 
     // Set title for the document
