21c89f32ea8a533d70dce695a53269c83cd08453
Tomo <thomas.gambier@gmail.com>
2012-03-05 19:33:21 +0100

    CLEANUP: indent

--- couchapps/admin/evently/bib_admin_div/_init/data.js
+++ couchapps/admin/evently/bib_admin_div/_init/data.js
@@ -1,13 +1,13 @@
 function(infos) {
-    // Set field in app so that everyone can access the site_id
-    var app = $$(this).app;
+  // Set field in app so that everyone can access the site_id
+  var app = $$(this).app;
 
-    copy_app_data(app, infos);
+  copy_app_data(app, infos);
 
-    $(this).trigger("post_changes");
+  $(this).trigger("post_changes");
 
-    // Set title for the document
-    document.title = "Administration Pointage";
+  // Set title for the document
+  document.title = "Administration Pointage";
 
   return;
 };
