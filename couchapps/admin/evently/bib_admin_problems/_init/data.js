760e04f34dd2580d9a121296535278663c996f8d
Baptiste Pernet <bapt@sp4ce.net>
2012-03-08 23:03:58 +0100

    Fix the ping request in admin page

--- couchapps/admin/evently/bib_admin_problems/_init/data.js
+++ couchapps/admin/evently/bib_admin_problems/_init/data.js
@@ -5,20 +5,20 @@ function(data) {
   var res = {};
   res.pbs = [];
   var pb_nb = 0;
-  var ping0 = data[0][0]["time"];
-  var ping1 = data[1][0]["time"];
-  var ping2 = data[2][0]["time"];
+  var ping0 = data[0][0]['max'];
+  var ping1 = data[1][0]['max'];
+  var ping2 = data[2][0]['max'];
 
   while (data[3][0].rows[i]) {
     var row = data[3][0].rows[i];
     var bib = row.key[0];
 
     if (bib != current_bib) {
-      $.log("Processing bib " + current_bib);
 
       // we can now check for previous bib
       res.pbs[pb_nb] = check_times(times, ping0, ping1, ping2);
-      if (res.pbs[pb_nb].site_id != undefined) { // there was a problem
+      // there was a problem
+      if (res.pbs[pb_nb].site_id != undefined) {
         res.pbs[pb_nb].bib = current_bib;
         pb_nb ++;
       }
@@ -38,9 +38,9 @@ function(data) {
   }
 
   // Check the last bib
-  $.log("Processing bib " + current_bib);
   res.pbs[pb_nb] = check_times(times, ping0, ping1, ping2);
-  if (res.pbs[pb_nb].site_id != undefined) { // there was a problem
+  // there was a problem
+  if (res.pbs[pb_nb].site_id != undefined) {
     res.pbs[pb_nb].bib = current_bib;
     pb_nb ++;
   }
