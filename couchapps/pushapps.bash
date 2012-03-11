240345e860f2d70e3a79e2a7ba741ac98538be01
Jon Harper <jon.harper87@gmail.com>
2012-03-11 18:18:20 +0100

    CLEANUP: use couchapp pushapps and pushdocs

--- couchapps/pushapps.bash
+++ couchapps/pushapps.bash
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-couchapps=$(ls -1 */.couchapprc | xargs -L1 dirname)
-for couchapp in $couchapps; do
-  echo "Pushing $couchapp"
-  (cd $couchapp && couchapp push)
-done
+dest="default"
+[[ "$1" != "" ]] && dest="$1"
+couchapp pushapps . $dest
+couchapp pushdocs _docs $dest
