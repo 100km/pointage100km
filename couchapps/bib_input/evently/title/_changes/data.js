923c76ec398b84aed09482e84c62784c76b9816a
Jon Harper <jon.harper87@gmail.com>
2012-03-24 14:27:46 +0100

    FEATURE: allow dynamic modification of filters setup by evently

--- couchapps/bib_input/evently/title/_changes/data.js
+++ couchapps/bib_input/evently/title/_changes/data.js
@@ -2,6 +2,7 @@ function(data) {
     // At this point, data (like site_id) has been copied in app
     var app = $$(this).app;
 
+    $.evently.changesDBs[app.db.name]["checkpoints"].opts.site_id = app.site_id;
     $(this).trigger("app_info_changed");
 
     // Set title for the document
--- couchapps/bib_input/filters/checkpoints.js
+++ couchapps/bib_input/filters/checkpoints.js
@@ -1,3 +1,4 @@
 function(doc, req) {
-  return doc.times;
+  var site_id = req.query.site_id;
+  return doc.times && (!site_id || site_id == doc.site_id);
 }
