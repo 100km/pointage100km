d41c276d7a3ccb46aedb60bb21b468a42aefdfed
Jon Harper <jon.harper87@gmail.com>
2012-03-28 18:34:25 +0200

    FEATURE: an empty string in a message doesn't make the orange box appear

--- couchapps/bib_input/evently/message-bar/_changes/data.js
+++ couchapps/bib_input/evently/message-bar/_changes/data.js
@@ -1,13 +1,22 @@
 function(data) {
-  res = data
   var all_empty = true;
-  for (i in res) {
-    var empty = res[i].length == 0;
-    res[i+"?"] = !empty;
+  for (i in data) {
+    var empty;
+    $.log("in " + i);
+    if (data[i].message != undefined) {
+      //Single object with message
+      empty = !data[i].message.length > 0;
+    } else {
+      //array of objects with messages
+      empty = !_.any(data[i], function(el) { return el.message.length > 0 });
+    }
+    data[i+"?"] = !empty;
     all_empty =  all_empty && empty;
   }
 
-  res.all_empty = all_empty
+  data.all_empty = all_empty;
+  $.log("final");
+  $.log(data);
 
-  return res;
+  return data;
 }
