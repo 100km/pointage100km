ec43e4496e48b94d2bdcc35e4028a71ab0dff1b8
Jon Harper <jon.harper87@gmail.com>
2012-03-28 17:29:18 +0200

    FEATURE: Add site_id as query parameter for messages filter

--- couchapps/common/filters/messages.js
+++ couchapps/common/filters/messages.js
@@ -1,3 +1,7 @@
 function(doc, req) {
-  return doc.message && (doc.deletedTS == undefined);
+  var site_id = req.query.site_id;
+  return doc.message && (doc.deletedTS == undefined)
+      && (doc.target != undefined ||
+          site_id != undefined ||
+          site_id == doc.target);
 }
