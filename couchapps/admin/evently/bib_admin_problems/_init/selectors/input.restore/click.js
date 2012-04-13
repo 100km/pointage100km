6a0eb5764aa87f17e471778c32501206f5e1d9cc
Baptiste Pernet <bapt@sp4ce.net>
2012-04-14 01:20:29 +0200

    Button to add the mising time when error is detected

---
+++ couchapps/admin/evently/bib_admin_problems/_init/selectors/input.restore/click.js
@@ -0,0 +1,19 @@
+function() {
+  var site_id = $(this).attr('data-site_id');
+  var lap = $(this).attr('data-lap');
+  var bib = $(this).attr('data-bib');
+  var db = $$(this).app.db;
+
+  db.openDoc('checkpoints-' + site_id + '-' + bib, {
+    success: function(doc) {
+      // Put the time in the deleted_times array.
+      var deleted_time = doc.deleted_times[lap];
+      doc.deleted_times = $.grep(doc.deleted_times, function(element) { return element != deleted_time; });
+      doc.times = doc.times || [];
+      doc.times.push(deleted_time);
+      doc.times.sort();
+      // Save the document.
+      db.saveDoc(doc);
+    }
+  });
+}
