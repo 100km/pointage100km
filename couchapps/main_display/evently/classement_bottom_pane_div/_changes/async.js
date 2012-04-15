dafabaed6b96001b5e164c319a1bdbd63a811775
sebastian <seba@mymail.com>
2012-04-15 06:39:00 -0400

    fix merge

--- couchapps/main_display/evently/classement_bottom_pane_div/_changes/async.js
+++ couchapps/main_display/evently/classement_bottom_pane_div/_changes/async.js
@@ -1,8 +1,9 @@
 function(cb) {
   var app = $$(this).app;
 
-  app.db.list("bib_input/global-ranking","global-ranking", {
+  app.db.list("main_display/global-ranking","global-ranking", {
     limit : 50,
+  },{
     success: function(data) {
       cb(data);
     }
