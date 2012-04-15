1d14b9c48e8ed9eae39864d32e3d9b6315018764
sebastian <seba@mymail.com>
2012-04-15 16:06:11 -0400

    left pane shows contestant info

---
+++ couchapps/common/views/all_contestants/map.js
@@ -0,0 +1,8 @@
+// View : all_contestants
+// Used to retrive the infos for the constestants, the goal being retrieving all the constestants at once
+// Used in ranking_title_div
+function(doc) {
+  if (doc.type == 'contestant' ) {
+      emit(doc.dossard, doc);
+  }
+};
