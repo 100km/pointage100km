f0a677e52f4c3f5666730ef27d1b7e3dbb4ebc7c
Thomas Gambier <thomas.gambier@gmail.com>
2012-04-16 22:26:49 +0200

    try to get a css defined table -like that of bib_input- to scroll

--- couchapps/main_display/evently/ranking_title_div/_init/data.js
+++ couchapps/main_display/evently/ranking_title_div/_init/data.js
@@ -8,6 +8,7 @@ function(data) {
     app.contestants = data;
 
     $(this).trigger("ranking_title_div_finish_load");
+    $(this).trigger("update_race_number_2");
 
     // Set title for the document
     document.title = "Classement2";
