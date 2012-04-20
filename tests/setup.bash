2ad904d341631d657ba118dd56f890ad6fcb1332
Baptiste Pernet <bapt@sp4ce.net>
2012-04-20 18:07:52 +0200

    Fixing some errors in the test couchApps script

--- tests/setup.bash
+++ tests/setup.bash
@@ -13,16 +13,17 @@ function pad_head() {
 
 which curl &> /dev/null || { echo "This script needs curl. Please install it." ; exit 1 ; }
 
-user=${2:-admin}
-password=${3:-admin}
+user=${1:-admin}
+password=${2:-admin}
 url="http://$user:$password@localhost:5984"
 test_db="test_db"
 echo "Deleting previous test database..."
 curl -s -X DELETE "$url/$test_db" -H "Content-Type: application/json" 2>&1 | pad_head 2
 
+echo "Pushing all the couch apps..."
 script_dir=$(cd $(dirname "$0"); pwd)
 couchapps_dir="$script_dir/../couchapps"
 (cd $couchapps_dir && ./server-pushapps local $user $password $test_db) 2>&1 | pad_head 2
 
 echo "Pushing test couchapp. Launch the tests at the following URL"
-(cd "$script_dir/test_couchapp" && couchapp push) 2>&1 | pad_head 2
+(cd "$script_dir/test_couchapp" && couchapp push $url/$test_db) 2>&1 | pad_head 2
