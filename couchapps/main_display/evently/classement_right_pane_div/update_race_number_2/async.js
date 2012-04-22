7d4630061c029d96da01c9e36fe68dfb17ccf14d
sebastian <seba@mymail.com>
2012-04-22 19:40:37 -0400

    add jquery.marquee and jquery.vticker to lib

--- couchapps/main_display/evently/classement_right_pane_div/update_race_number_2/async.js
+++ couchapps/main_display/evently/classement_right_pane_div/update_race_number_2/async.js
@@ -1,6 +1,8 @@
 function(cb) {
-  var app = $$(this).app;
+    var app = $$(this).app;
+    
+    $.log("right_pane_async");
 
-  db_global_ranking(app, cb, 2);
+    db_global_ranking(app, cb, 2);
 };
 
