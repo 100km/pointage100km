97ee760caa401af50f8cbb91f87262f162d3a6a5
Jon Harper <jon.harper87@gmail.com>
2012-04-22 15:04:23 +0200

    Use jquery-ui-extension to have the searched pattern in bold

--- couchapps/search/evently/search/_init/selectors/input/_init/before.js
+++ couchapps/search/evently/search/_init/selectors/input/_init/before.js
@@ -1,15 +1,19 @@
 function() {
   var app = $$(this).app;
   $(this).autocomplete({
+    html: true,
     source: function(request, response) {
       var term = request.term;
       app.db.view("search/contestants-search", {
         limit: 10,
-        startkey: request.term,
-        endkey: increment_string_key(request.term),
+        startkey: term,
+        endkey: increment_string_key(term),
         success: function(data) {
           response(data.rows.map(function(row) {
-            return row.key;
+            return {
+              label: "<strong>"+row.key.substring(0,term.length)+"</strong>"+row.key.substring(term.length),
+              value:row.key
+            };
           }));
         }
       })
