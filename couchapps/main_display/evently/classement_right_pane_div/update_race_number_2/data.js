cf3a32352953cdbbd779d05fbb99631ba9256733
sebastian <seba@mymail.com>
2012-04-22 19:40:37 -0400

    add jquery.marquee and jquery.vticker to lib

--- couchapps/main_display/evently/classement_right_pane_div/update_race_number_2/data.js
+++ couchapps/main_display/evently/classement_right_pane_div/update_race_number_2/data.js
@@ -1,17 +1,19 @@
 function(data) {
-  var p;
+    var p;
 
-  return {
-    races: data.rows.map(function(pair) {
-      return {
-        race_id: pair.race_id,
-        items: pair.contestants.map(function(r) {
-          p = {};
-          p.bib = r.value && r.value.bib;
-          p.lap = r.value && r.value.times.length;
-          return p;
-        })
-      }
-    })
-  }
+    $.log("right_pane_data");
+
+    return {
+	races: data.rows.map(function(pair) {
+	    return {
+		race_id: pair.race_id,
+		items: pair.contestants.map(function(r) {
+		    p = {};
+		    p.bib = r.value && r.value.bib;
+		    p.lap = r.value && r.value.times.length;
+		    return p;
+		})
+	    }
+	})
+    }
 };
