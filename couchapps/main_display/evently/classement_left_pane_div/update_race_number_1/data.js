e56c86516bb21f0add7fab86d8cc7539ba6ed6b6
Thomas Gambier <thomas.gambier@gmail.com>
2012-04-22 14:18:13 +0200

    Much simpler function and add rank in mustache

--- couchapps/main_display/evently/classement_left_pane_div/update_race_number_1/data.js
+++ couchapps/main_display/evently/classement_left_pane_div/update_race_number_1/data.js
@@ -1,85 +1,33 @@
 function(data) {
   var p = {};
-  var res = [];
+  p.items = [];
   var i = 0;
-  var app = $$(this).app
+  var app = $$(this).app;
 
-  $.log("app.contestants: " + JSON.stringify(app.contestants));
+  while (data.rows[0].contestants[i]) {
+    var item = {};
+    var current_infos = data.rows[0].contestants[i].value;
+    var current_contestant = app.contestants[current_infos.bib];
 
-  //$.log("data: " + JSON.stringify(data));
-
-  function map_contestants(data) {
-    var result = {};
-
-    //$.log("contestants: " + JSON.stringify(data));
-
-    if (data.value) {
-      result.bib = data.value.bib;
-      result.lap = data.value.times.length;
-
-      //unroll the "multi-row" stuff (don't understand the point of having it like that yet)
-      res.push(result);
-      i++;
+    //$.log("current_infos = " + JSON.stringify(current_infos));
+    if (current_contestant === undefined) {
+      $.log("skip contestant: " + data.bib);
     }
 
-    return result;
+    item.rank = i;
+    item.dossard = current_infos.bib;
+    item.lap = current_infos.lap
+    item.nom     = current_contestant.nom;
+    item.prenom  = current_contestant.prenom;
+    item.course  = current_contestant.course;
+
+    p.items.push(item);
+    i++;
   }
-
-  function map_races(data) {
-    var result = {};
-
-    //$.log("races: " + JSON.stringify(data));
-
-    //result.race_id = data.race_id;
-    result = data.contestants.map(map_contestants);
-
-    return result;
-  }
-
-  function map_bib(data) {
-    var result = {
-      bib: 0,
-      dossard: 0,
-      nom: "",
-      prenom: "",
-      course: 0,
-      lap: 0,
-    };
-
-    $.log("bibs: " + JSON.stringify(data));
-
-    //set known values
-    result.dossard = data.bib;
-    result.lap     = data.lap;
-
-    if (app.contestants === undefined) {
-      $.log("contestants not loaded yet");
-      return;
-    }
-    else {
-      var current_contestant = app.contestants[data.bib];
-
-      if (current_contestant === undefined) {
-        $.log("skip contestant: " + data.bib);
-      }
-      else {
-        result.nom     = current_contestant.nom;
-        result.prenom  = current_contestant.prenom;
-        result.course  = current_contestant.course;
-      }
-    }
-
-    return result;
-  }
-
-  data.rows.map(map_races);
 
   p.count = i;
-  p.items = res.map(map_bib);
 
-  $.log("res: " + JSON.stringify(res));
   $.log("p: " + JSON.stringify(p));
 
   return p;
-
 };
