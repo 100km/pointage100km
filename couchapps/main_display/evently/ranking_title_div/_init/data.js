aa602e077b6f207a024d32588dceea25ef34cf1f
sebastian <seba@mymail.com>
2012-04-23 16:05:11 -0400

    update bottom pane

--- couchapps/main_display/evently/ranking_title_div/_init/data.js
+++ couchapps/main_display/evently/ranking_title_div/_init/data.js
@@ -1,13 +1,14 @@
 function(data) {
-    var app = $$(this).app;
-
-    // Set field in app so that everyone can access the contestants infos
-    app.contestants = data;
-
-    $(this).trigger("update_race_number_1");
-    $(this).trigger("update_race_number_2");
-
-    $.log("triggering events");
-
-    return;
+  var app = $$(this).app;
+  
+  // Set field in app so that everyone can access the contestants infos
+  app.contestants = data;
+  
+  $(this).trigger("update_race_number_1");
+  $(this).trigger("update_race_number_2");
+  //$(this).trigger("_changes");
+  
+  $.log("triggering events");
+  
+  return;
 };
