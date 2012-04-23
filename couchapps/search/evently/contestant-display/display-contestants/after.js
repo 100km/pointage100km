ddde5a7e554da8de84b95e8394e6086793578829
Jon Harper <jon.harper87@gmail.com>
2012-04-23 20:49:06 +0200

    CLEANUP: search, pass only required data

--- couchapps/search/evently/contestant-display/display-contestants/after.js
+++ couchapps/search/evently/contestant-display/display-contestants/after.js
@@ -1,6 +1,6 @@
 function(data) {
   if (data.length == 1)
-    $(this).trigger("single-contestant", data[0]);
+    $(this).trigger("single-contestant", data[0].value);
   else
-    $(this).trigger("multiple-contestants", {names:data});
+    $(this).trigger("multiple-contestants", {names:data.map(function(row) { return row.value })});
 }
