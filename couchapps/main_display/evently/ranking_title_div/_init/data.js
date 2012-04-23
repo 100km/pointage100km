293bc6bab7c3de4e4f5833473f010fa212183a54
Thomas Gambier <thomas.gambier@gmail.com>
2012-04-23 20:35:49 +0200

    Add infos about kms and time

--- couchapps/main_display/evently/ranking_title_div/_init/data.js
+++ couchapps/main_display/evently/ranking_title_div/_init/data.js
@@ -1,12 +1,12 @@
 function(data) {
     var app = $$(this).app;
-    
+
     // Set field in app so that everyone can access the contestants infos
     app.contestants = data;
-    
+
     $(this).trigger("update_race_number_1");
     $(this).trigger("update_race_number_2");
-    
+
     $.log("triggering events");
 
     return;
