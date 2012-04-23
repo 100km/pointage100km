7a7be0975105a4ad6cf3694cf6f04ce02a0c6907
Jon Harper <jon.harper87@gmail.com>
2012-04-23 23:16:31 +0200

    BUGFIX: search, handle searches for 'name firstname' or 'firstname name' better

--- couchapps/search/evently/search/_init/selectors/input/_init/before.js
+++ couchapps/search/evently/search/_init/selectors/input/_init/before.js
@@ -3,26 +3,32 @@ function() {
   $(this).autocomplete({
     html: true,
     source: function(request, response) {
-      var term = request.term;
+      var split = request.term.split(" ");
+      var term = split[0];
       app.db.view("search/contestants-search", {
         limit: 10,
         startkey: term,
         endkey: increment_string_key(term),
         success: function(data) {
+        var regexp = new RegExp("("+term+")","i");
+        var regexp2 = new RegExp("( "+split[1]+")","i");
           response(data.rows.map(function(row) {
-            var value = row.value.nom + " " + row.value.prenom;
-            var match = row.value[row.value.match];
-            var label = "<strong>"+match.substring(0,term.length)+"</strong>"+match.substring(term.length);
-            if (row.value.match == "nom") {
-              label = label + " " + row.value.prenom;
-            } else {
-              label = row.value.nom + " " + label;
+            var value = row.value[row.value.match] + " " +
+                        row.value[search_nonmatch_field(row.value.match)];
+            var label = value.replace(regexp, "<strong>$1</strong>");
+            if (split.length>1) {
+              var label2 = label.replace(regexp2, "<strong>$1</strong>");
+              if (label2 != label) {
+                label = label2;
+              } else {
+                return {};
+              }
             }
             return {
               label: label,
               value: value
             };
-          }));
+          }).filter(function(data) { return data.value != undefined; }));
         }
       });
     }
