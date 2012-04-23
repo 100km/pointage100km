a5148deec8f1640aedd79c0b277d106c9a5dc783
Jon Harper <jon.harper87@gmail.com>
2012-04-23 21:08:38 +0200

    FEATURE: search; close autocomplete when submitting

--- couchapps/search/evently/search/_init/selectors/form/submit.js
+++ couchapps/search/evently/search/_init/selectors/form/submit.js
@@ -2,5 +2,9 @@ function() {
   var app = $$(this).app;
   var req = this.input.value;
   $(this).trigger("display-contestants", req);
+  var input = $(this).find(".ui-autocomplete-input");
+  if (input && input.autocomplete( "widget" ).is( ":visible" ) ) {
+    input.autocomplete( "close" );
+  }
   return false;
 };
