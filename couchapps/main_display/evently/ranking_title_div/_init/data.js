1410d8f0803d5ac4c9c638c8a894f69b10ba4a2b
Thomas Gambier <thomas.gambier@gmail.com>
2012-04-25 22:17:36 +0200

    BUGFIX: correctly index contestants infos by bib

--- couchapps/main_display/evently/ranking_title_div/_init/data.js
+++ couchapps/main_display/evently/ranking_title_div/_init/data.js
@@ -2,11 +2,14 @@ function(data) {
   var app = $$(this).app;
 
   // Set field in app so that everyone can access the contestants infos
-  app.contestants = data;
+  app.contestants = [];
+  _.each(data, function (item, index) {
+    // $.log("In foreach : bib = " + item.dossard + " item = " + JSON.stringify(item));
+    app.contestants[item.dossard] = item;
+  });
 
   $("#left_pane_div").trigger("update_ranking", {race_id:1});
   $("#right_pane_div").trigger("update_ranking", {race_id:2});
-  //$(this).trigger("_changes");
 
   return;
 };
