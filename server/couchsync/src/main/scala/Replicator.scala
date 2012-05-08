d8acff94f0fb1ca6dde15736a4f28eec63539cdf
Samuel Tardieu <sam@rfc1149.net>
2012-05-08 17:37:25 +0200

    Extract SlaveDb to where it belongs

--- server/couchsync/src/main/scala/Replicator.scala
+++ server/couchsync/src/main/scala/Replicator.scala
@@ -1,12 +1,9 @@
 import akka.actor.ActorSystem
 import akka.dispatch.{Await, Future}
 import akka.util.duration._
-import java.io.{File, FileWriter}
-import java.lang.{Process, ProcessBuilder}
 import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
 import net.rfc1149.canape._
-import scala.io.Source
 import steenwerck._
 
 object Replicator {
@@ -42,7 +39,7 @@ object Replicator {
 
     step("construction de la configuration")
 
-    val c = new Replicator(options.localDir, options.usbDir)
+    val c = new SlaveDb(options.localDir, options.usbDir)
     c.runCouchDb()
     val couch = c.couch
     wait("lancement de la base sur clé USB", 30) {
@@ -90,65 +87,3 @@ object Replicator {
   }
 }
 
-class Replicator(srcDir: File, usbBaseDir: File) {
-
-  import Replicator._
-
-  val dbDir = new File(usbBaseDir, "db")
-  val etcDir = new File(usbBaseDir, "etc")
-  val runDir = new File(usbBaseDir, "run")
-
-  private[this] var process: Option[Process] = None
-
-  val defaultFile = new File(etcDir, "default.ini")
-  val logFile = new File(runDir, "couchdb.log")
-  val pidFile = new File(runDir, "couchdb.pid")
-  val uriFile = new File(runDir, "couchdb.uri")
-
-  private[this] var _couch: Option[Couch] = None
-
-  dbDir.mkdirs()
-  etcDir.mkdirs()
-  runDir.mkdirs()
-
-  def fixDefaultIni() {
-    val ini = new Ini
-
-    ini.load(Source.fromFile(new File(srcDir, "default.ini")))
-    ini.load(Source.fromFile(new File(srcDir, "local.ini")))
-
-    ini.set("couchdb", "database_dir", dbDir)
-    ini.set("couchdb", "delayed_commits", false)
-    ini.set("couchdb", "view_index_dir", dbDir)
-    ini.set("couchdb", "uri_file", uriFile)
-    ini.set("httpd", "bind_address", "127.0.0.1")
-    ini.set("httpd", "port", 5983)
-    ini.set("log", "file", logFile)
-    // ini.set("log", "level", "debug")
-
-    val out = new FileWriter(defaultFile)
-    ini.save(out)
-    out.close()
-  }
-
-  def couch = _couch.get
-
-  def runCouchDb() {
-    fixDefaultIni()
-
-    val pb = new ProcessBuilder("couchdb",
-				"-n", "-a", defaultFile.toString,
-				"-p", pidFile.toString)
-    pb.directory(usbBaseDir)
-    process = Some(pb.start())
-
-    _couch = Some(new NioCouch("localhost", 5983, Some("admin", "admin")))
-  }
-
-  def stopCouchDb() {
-    process foreach (_.destroy())
-    process = None
-    _couch = None
-  }
-
-}
