bffeebf9b3ebbad569163207bc3b8d4eb146a8b3
Baptiste Pernet <bapt@sp4ce.net>
2012-05-10 23:57:10 +0200

    Clean the delete message feature

--- couchapps/admin/evently/bib_admin_message_list_div/_changes/selectors/form/submit.js
+++ couchapps/admin/evently/bib_admin_message_list_div/_changes/selectors/form/submit.js
@@ -1,61 +1,32 @@
 function() {
-    var app = $$(this).app;
-    var form = $(this)[0];
-    var message_to_modify = {};
-    var message_to_delete = {};
-    var message_id      = form["id"].value;
-    var message_rev     = form["rev"].value;
-    var message_target  = form["target"].value;
-    var message_body    = form["body"].value;
-    var message_addedTS = form["addedTS"].value;
+  var app = $$(this).app;
+  var form = $(this)[0];
+  var message_id = form["id"].value;
 
-    form.reset();
+  form.reset();
 
-    message_target = parseInt(message_target, 10); //hackish, message_target changes type from string to int
-    message_addedTS = parseInt(message_addedTS, 10); //hackish, message_addedTS changes type from string to int
+  // Callback on the modification of the message.
+  function callback_error(stat, err, reason) {
+    $.log("stat" + JSON.stringify(stat));
+    $.log("err" + JSON.stringify(err));
+    $.log("reason" + JSON.stringify(reason));
+    alert("Error adding message to db: " + JSON.stringify(reason));
+  }
 
-    $.log("clicked on message: id='" + message_id + "', body='" + message_body + "' for target=" + message_target);
+  // Save the modification message.
+  function mark_message_as_deleted() {
+    $.log("marking as deleted");
+    app.db.openDoc(message_id, {
+      success: function(doc) {
+        doc.deletedTS = new Date().getTime();
+        app.db.saveDoc(doc, { error: callback_error });
+      },
+      error: callback_error
+    });
+  }
 
-    //to use saveDoc and update an existing document, _id and _rev are required, as well as the rest of the fields (don't know if I could just omit those that do not need to be updated)
-    message_to_modify._id       = message_id;
-    message_to_modify._rev      = message_rev;
-    message_to_modify.message   = message_body;
-    if (!isNaN(message_target)) {
-	message_to_modify.target    = message_target;
-    }
-    message_to_modify.addedTS   = message_addedTS;
-    message_to_modify.deletedTS = new Date().getTime();
+  // Hide the line and then mark the message as deleted.
+  $(this).parents("li").hide('fast', mark_message_as_deleted);
 
-    //to use removeDoc, only _id and _rev are required
-    message_to_delete._id       = message_id;
-    message_to_delete._rev      = message_rev;
-
-    function callback_error(stat, err, reason) {
-	$.log("stat" + JSON.stringify(stat));
-	$.log("err" + JSON.stringify(err));
-	$.log("reason" + JSON.stringify(reason));
-	alert("Error adding message to db: " + JSON.stringify(reason));
-    }
-
-    function callback_ok() {
-	$.log("ok");
-    }
-
-
-    function mark_message_as_deleted() {
-	$.log("marking as deleted");
-
-	app.db.saveDoc(message_to_modify, { success: callback_ok, error: callback_error });
-    }
-
-    function delete_message() {
-	$.log("deleting");
-
-	app.db.removeDoc(message_to_delete, { success: callback_ok, error: callback_error });
-    }
-
-    $(this).parents("li").hide('fast', mark_message_as_deleted());
-
-    return false;
-
+  return false;
 };
