b8f3b102ea9caa18a480211e7cc39b25eed9331e
Thomas Gambier <thomas.gambier@gmail.com>
2012-05-10 23:50:15 +0200

    Let the deleted messages trigger a change

--- couchapps/common/filters/messages.js
+++ couchapps/common/filters/messages.js
@@ -1,6 +1,6 @@
 function(doc, req) {
   var site_id = req.query.site_id;
-  return (doc.message || doc.message == "") && (doc.deletedTS == undefined)
+  return (doc.message || doc.message == "")
       && (doc.target != undefined ||
           site_id != undefined ||
           site_id == doc.target);
