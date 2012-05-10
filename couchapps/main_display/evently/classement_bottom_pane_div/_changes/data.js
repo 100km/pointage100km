facfbf4982517b758276b022bf89432722cb4b14
Thomas Gambier <thomas.gambier@gmail.com>
2012-05-11 01:02:01 +0200

    Display more information at the bottom since we have more space

--- couchapps/main_display/evently/classement_bottom_pane_div/_changes/data.js
+++ couchapps/main_display/evently/classement_bottom_pane_div/_changes/data.js
@@ -15,11 +15,17 @@ function(data) {
     p.lap = r.value && r.value.lap;
     p.ts  = r.key[1];
     p.time_hour = time_to_hour_string(p.ts);
+    p.kms = site_lap_to_kms(app, 2, p.lap);
 
     p.nom = r.infos && r.infos.nom;
     p.prenom = r.infos && r.infos.prenom;
-    p.course = r.infos && r.infos.course;
+    p.race = r.infos && r.infos.course;
 
+    var time_to_convert = p.ts - app.start_times[p.race];
+    p.global_average = p.kms * 1000 * 3600 / time_to_convert;
+    p.global_average = p.global_average.toFixed(2);
+
+    p.race_name = app.races_names[p.race];
     return p;
   }
 
