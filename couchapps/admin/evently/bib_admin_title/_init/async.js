3b599e118011986fa2047bdac547c466e604734d
Thomas Gambier <thomas.gambier@gmail.com>
2012-05-10 21:38:50 +0200

    Add last ping for each site on admin page

--- couchapps/admin/evently/bib_admin_title/_init/async.js
+++ couchapps/admin/evently/bib_admin_title/_init/async.js
@@ -1,6 +1,13 @@
 function(cb) {
   var app = $$(this).app;
 
-  get_doc(app, cb, "infos");
+  fork([
+    function(cb) { get_ping(app, 0, cb) },
+    function(cb) { get_ping(app, 1, cb) },
+    function(cb) { get_ping(app, 2, cb) },
+    function(cb) { get_doc(app, cb, "infos") }
+  ],
+  cb);
+
 }
 
