4dd9018d837b401bdc8cc4fc193706fea7c75eeb
Thomas Gambier <thomas.gambier@gmail.com>
2012-05-13 00:14:07 +0200

    Try to switch to race 4 if there is no times in race 2

--- couchapps/main_display/evently/classement_side_pane_div/update_ranking/async.js
+++ couchapps/main_display/evently/classement_side_pane_div/update_ranking/async.js
@@ -1,5 +1,21 @@
 function(cb) {
   var app = $$(this).app;
   var race_id = parseInt(this[0].getAttribute("data-race_id"));
-  db_global_ranking(app, cb, race_id);
+
+  // If race_id is 2 and there is no timestamps for race_id 2, go to race_id 4
+  if (race_id == 2) {
+    app.db.list("main_display/global-ranking","global-ranking", {
+      startkey:[race_id, null, null, null],
+      endkey:[race_id+1, null, null, null]
+    }, {
+      success: function(data) {
+        if (data.rows.length > 0)
+          db_global_ranking(app, cb, race_id);
+        else
+          db_global_ranking(app, cb, 4);
+        }
+    });
+  }
+  else
+    db_global_ranking(app, cb, race_id);
 };
