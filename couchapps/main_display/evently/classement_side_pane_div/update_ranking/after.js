7da727b5dc3254d0456b481cf2747ac3f31bb68a
Thomas Gambier <thomas.gambier@gmail.com>
2012-05-13 11:54:04 +0200

    Let the first ones displayed during 5 secons before scrolling

--- couchapps/main_display/evently/classement_side_pane_div/update_ranking/after.js
+++ couchapps/main_display/evently/classement_side_pane_div/update_ranking/after.js
@@ -1,14 +1,16 @@
 function() {
   var _this = this;
-  $(this).find("#ranking-container").vTicker({
-    speed: 500,
-    pause: 1000,
-    animation: 'fade',
-    mousePause: false,
-    showItems: 10
-  }, function() {
-    setTimeout( function() {
-      $(_this).trigger("update_ranking");
-    }, 2000);
-  });
+  setTimeout( function() {
+    $(_this).find("#ranking-container").vTicker({
+      speed: 500,
+      pause: 1000,
+      animation: 'fade',
+      mousePause: false,
+      showItems: 10
+    }, function() {
+      setTimeout( function() {
+        $(_this).trigger("update_ranking");
+      }, 2000);
+    });
+  }, 5000);
 }
