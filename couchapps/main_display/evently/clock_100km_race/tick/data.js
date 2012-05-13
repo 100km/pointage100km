8868cad3604fde3c67534057b23d476ce7330f98
Thomas Gambier <thomas.gambier@gmail.com>
2012-05-13 02:37:52 +0200

    Use infos in app only when initialized

--- couchapps/main_display/evently/clock_100km_race/tick/data.js
+++ couchapps/main_display/evently/clock_100km_race/tick/data.js
@@ -1,6 +1,10 @@
 function(data) {
   var race_id = parseInt(this[0].getAttribute("data-race_id"));
   var app = $$(this).app;
+
+  if (!appinfo_initialized(app))
+    return {};
+
   var open_date_offset = app.start_times[race_id];
   var race_name = app.races_names[race_id];
   var res = { race_name: race_name };
