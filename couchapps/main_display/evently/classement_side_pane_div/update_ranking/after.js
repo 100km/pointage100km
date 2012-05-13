8f51d0c84d147e3023ee84211caff7dcdf75f3b4
Thomas Gambier <thomas.gambier@gmail.com>
2012-05-13 23:28:33 +0200

    When the race 4 is displayed, use blue color

--- couchapps/main_display/evently/classement_side_pane_div/update_ranking/after.js
+++ couchapps/main_display/evently/classement_side_pane_div/update_ranking/after.js
@@ -1,5 +1,11 @@
-function() {
+function(data) {
   var _this = this;
+
+  if (data.race_id == 4)
+    $(_this).addClass('race_4');
+  else
+    $(_this).removeClass('race_4');
+
   $(this).find("#ranking-container").vTicker({
     speed: 500,
     pause: 1000,
