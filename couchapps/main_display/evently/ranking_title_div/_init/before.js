b36e2e42ad5718901c4cac1d61def8fc1d9027ec
Thomas Gambier <thomas.gambier@gmail.com>
2012-05-13 03:11:56 +0200

    Correct ranking_title_div

---
+++ couchapps/main_display/evently/ranking_title_div/_init/before.js
@@ -0,0 +1,36 @@
+function(cb) {
+  var app = $$(this).app;
+  var _this = this;
+
+  function set_contestant(data) {
+    var race_id = parseInt(_this[0].getAttribute("data-race_id"));
+
+    // Set field in app so that everyone can access the contestants infos
+    app.contestants = [];
+    _.each(data, function (item, index) {
+      // $.log("In foreach : bib = " + item.dossard + " item = " + JSON.stringify(item));
+      app.contestants[item.dossard] = item;
+    });
+
+    if (race_id) {
+      // We are in the title for only one race
+      $("#single_ranking_div").trigger("update_ranking", {race_id:race_id});
+    }
+    else {
+      $("#left_pane_div").trigger("update_ranking", {race_id:1});
+      $("#right_pane_div").trigger("update_ranking", {race_id:2});
+    }
+
+    return;
+  };
+
+
+  fork([
+    function(cb) { db_get_all_contestants(app, cb) },
+    function(cb) { db_app_data(app, cb) }
+  ], function(result) {
+    set_contestant(result[0][0]);
+  });
+
+}
+
