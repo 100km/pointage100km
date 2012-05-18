549db0fa3c66f4cbd0858ecc6077bdbb6911ec51
Thomas Gambier <thomas.gambier@gmail.com>
2012-05-18 14:02:19 +0200

    Fix list to import in mdb databases

--- couchapps/search/lists/times-to-csv.js
+++ couchapps/search/lists/times-to-csv.js
@@ -7,16 +7,26 @@ function(head, req) {
   });
 
   var row;
-  while(row = getRow()) {
-    for (var i=0; i<row.value.times.length; i=i+1) {
-    var sec = Math.floor(row.value.times[i] / 1000);
-    var min = Math.floor(sec / 60);
-    var hour = Math.floor(min / 60);
-    var date_str = [hour % 24, min % 60, sec % 60].map(function (n) {
-      return ((n < 10) && (n >= 0) ? '0' : '') + n
-    }).join(":");
+  var race_id;
+  // first doc contains the infos
+  row = getRow()
+  var start_times = row.value.start_times;
 
-    send([row.value.bib, date_str, row.value.site_id, i].join(";") + "\n");
+  while(row = getRow()) {
+    // We get the race of the bib
+    race_id = row.doc.course;
+
+    row = getRow();
+    for (var i=0; i<row.value.times.length; i=i+1) {
+      var absolute_time = row.value.times[i] - start_times[race_id];
+      var sec = Math.floor(absolute_time / 1000);
+      var min = Math.floor(sec / 60);
+      var hour = Math.floor(min / 60);
+      var date_str = [hour, min % 60, sec % 60].map(function (n) {
+        return ((n < 10) && (n >= 0) ? '0' : '') + n
+      }).join(":");
+
+      send([row.value.bib, date_str, row.value.site_id+1, i+1].join(";") + "\r\n");
     }
   }
 
---
+++ couchapps/search/views/all-times-and-infos/map.js
@@ -0,0 +1,10 @@
+function(doc) {
+  if (doc._id == "infos")
+    emit(false, doc);
+
+  if (doc.bib!=undefined && doc.times && doc.site_id!=undefined) {
+    emit([doc.bib, doc.site_id, false], {_id:"contestant-" + doc.bib});
+
+    emit([doc.bib, doc.site_id, true], doc);
+  }
+}
