4becefebf0d4191cafbaeb5017106cf1bd060a00
Thomas Gambier <thomas.gambier@gmail.com>
2013-04-23 00:44:28 +0200

    Admin application: fix pings.

--- couchapps/admin/evently/bib_admin_title/_init/async.js
+++ couchapps/admin/evently/bib_admin_title/_init/async.js
@@ -2,10 +2,14 @@ function(cb) {
   var app = $$(this).app;
 
   fork([
+    function(cb) { get_doc(app, cb, "infos") },
     function(cb) { get_ping(app, 0, cb) },
     function(cb) { get_ping(app, 1, cb) },
     function(cb) { get_ping(app, 2, cb) },
-    function(cb) { get_doc(app, cb, "infos") }
+    function(cb) { get_ping(app, 3, cb) },
+    function(cb) { get_ping(app, 4, cb) },
+    function(cb) { get_ping(app, 5, cb) },
+    function(cb) { get_ping(app, 6, cb) }
   ],
   cb);
 
