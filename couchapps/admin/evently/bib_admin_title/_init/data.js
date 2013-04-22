dc51dfdeec3a26687a286aa0e7c6efdd3ccac849
Thomas Gambier <thomas.gambier@gmail.com>
2013-04-23 00:44:28 +0200

    Admin application: fix pings.

--- couchapps/admin/evently/bib_admin_title/_init/data.js
+++ couchapps/admin/evently/bib_admin_title/_init/data.js
@@ -2,7 +2,7 @@ function(datas) {
   // Set field in app so that everyone can access the site_id
   var app = $$(this).app;
   var pings = [];
-  var infos = datas[3][0];
+  var infos = datas[0][0];
 
   copy_app_data(app, infos);
 
@@ -12,8 +12,8 @@ function(datas) {
   document.title = "Administration Pointage";
 
   // Set ping infos
-  for (i=0; i<3; i++)
-    pings[i] = {ping : format_date(new Date(datas[i][0])), name : app.sites[i]};
+  for (i=0; i<app.sites_nb; i++)
+    pings[i] = {ping : format_date(new Date(datas[i+1][0])), name : app.sites[i]};
 
   return {"pings" : pings};
 };
