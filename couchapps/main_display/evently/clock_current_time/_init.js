1c23d7f368e67081c9fb2bf6886485d073ad5149
Jon Harper <jon.harper87@gmail.com>
2013-04-23 02:09:34 +0200

    BUGFIX: workaround jquery 1.6 bug in chrome

--- couchapps/main_display/evently/clock_current_time/_init.js
+++ couchapps/main_display/evently/clock_current_time/_init.js
@@ -1,6 +1,9 @@
 function() {
   var _this = this;
   $(_this).trigger("tick")
+  //see http://bugs.jquery.com/ticket/9678
+  var dummy_interval = setInterval(function () {
+  }, 100000);
   setInterval(function() {
     $(_this).trigger("tick")
   }, 1000);
