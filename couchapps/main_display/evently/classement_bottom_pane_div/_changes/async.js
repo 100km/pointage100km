7cb0fd4c6f7a773b833277036f14a9bc0ec2d0da
Jon Harper <jon.harper87@gmail.com>
2013-04-27 17:58:28 +0200

    cleanup duplicate code

--- couchapps/main_display/evently/classement_bottom_pane_div/_changes/async.js
+++ couchapps/main_display/evently/classement_bottom_pane_div/_changes/async.js
@@ -8,26 +8,8 @@ function(cb) {
 
   var site_id = app.sites_nb - 1;
 
-  // startkey and endkey are inversed because descending is true
-  app.db.view("bib_input/recent-checkpoints", {
-    descending: true,
-    include_docs: true,
-    limit : 5,
-    startkey : [(site_id+1),0],
-    endkey : [site_id,0],
-    success: function(data) {
-      _.each(data.rows, function(row) {
-        if (row.doc) {
-          row.infos = {
-            first_name: row.doc.first_name,
-            name: row.doc.name,
-            race: row.doc.race
-          };
-        }
-      });
-
-      cb({site_id: site_id, data: data.rows});
-    }
-  });
+  db_recent(app, function(data) {
+    cb({site_id: site_id, data: data});
+  }, site_id, 5);
 };
 
