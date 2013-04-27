68f038bf3b8eb9fbc4905b547660193f93b696e3
Jon Harper <jon.harper87@gmail.com>
2013-04-27 17:33:34 +0200

    Don't display race 2 if it has not started; fixes #72

--- couchapps/main_display/evently/classement_side_pane_div/update_ranking/async.js
+++ couchapps/main_display/evently/classement_side_pane_div/update_ranking/async.js
@@ -2,20 +2,19 @@ function(cb) {
   var app = $$(this).app;
   var race_id = parseInt(this[0].getAttribute("data-race_id"));
 
-  // If race_id is 2 and there is no timestamps for race_id 2, go to race_id 4
-  if (race_id == 2) {
-    app.db.list("main_display/global-ranking","global-ranking", {
-      startkey:[race_id, null, null, null],
-      endkey:[race_id+1, null, null, null]
-    }, {
-      success: function(data) {
-        if (data.rows.length > 0)
-          db_global_ranking(app, cb, race_id);
-        else
-          db_global_ranking(app, cb, 3);
-        }
-    });
-  }
-  else
-    db_global_ranking(app, cb, race_id);
+  var now = new Date().getTime();
+  // When race 2 has not started yet, display race 3 instead
+  if (race_id == 2 && app.start_times[2] > now)
+    race_id = 3;
+
+  db_global_ranking(app, function(data) {
+      // Here, if race_id is 2, then the race 2 has started.
+      // If race_id is 2 and there is no timestamps for race_id 2, go to race_id 3
+      console.log("data is", data);
+      if (race_id == 2 && data.data.rows.length == 0) {
+        db_global_ranking(app, cb, 3);
+      } else {
+        cb(data);
+      }
+  }, race_id);
 };
