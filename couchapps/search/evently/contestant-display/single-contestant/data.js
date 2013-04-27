5713b5547dfcf8e350252a4f9f5fe30dae9306b9
Jon Harper <jon.harper87@gmail.com>
2013-04-27 20:20:39 +0200

    Search: fixes for number of sites; don't show results out of laps

--- couchapps/search/evently/contestant-display/single-contestant/data.js
+++ couchapps/search/evently/contestant-display/single-contestant/data.js
@@ -3,15 +3,15 @@ function(data) {
 
   var result = {};
   result.infos = data.infos;
-  result.times = [];
-  for(var i= 0; i < data.times.length; i++) {
-    for(var j= 0; j < data.times[i].times.length; j++) {
-      result.times[j*3 + i] = {};
-      result.times[j*3 + i].time = time_to_hour_string(data.times[i].times[j]);
-      result.times[j*3 + i].site = app.sites[data.times[i].site_id];
-      result.times[j*3 + i].kms  = site_lap_to_kms(app, data.times[i].site_id, j + 1);
-      result.times[j*3 + i].lap  = j + 1;
-    }
-  }
+  result.times = _.map(data.times.rows, function(time) {
+    var res = {}
+    var lap = time.key[1];
+    var site_id = time.key[2];
+    res.time = time_to_hour_string(time.value);
+    res.site = app.sites[site_id];
+    res.kms  = site_lap_to_kms(app, site_id, lap);
+    res.lap  = lap;
+    return res;
+  });
   return result;
 }
--- couchapps/search/views/all-times-per-bib/map.js
+++ couchapps/search/views/all-times-per-bib/map.js
@@ -1,6 +1,11 @@
-//Used in search
+// View : all-times-per-bib
+// Used to display the times of a contestant (search)
 //TODO teams
 function(doc) {
-  if (doc.bib!=undefined && doc.times && doc.site_id!=undefined)
-    emit(doc.bib, doc);
-}
+  if (doc.bib != undefined && doc.times && doc.site_id != undefined) {
+    var len = doc.times.length;
+    for (var i=0; i<len; i=i+1) {
+      emit([doc.bib, i+1, doc.site_id], doc.times[i]);
+    }
+  }
+};
