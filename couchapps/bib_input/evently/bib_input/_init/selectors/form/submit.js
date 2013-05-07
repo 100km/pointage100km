74c802dbb89d1a9d9317089360d20b7388e5f97d
Jon Harper <jon.harper87@gmail.com>
2013-05-08 00:45:52 +0200

    bib_input Prevent inserting checkpoint-undefined-X

--- couchapps/bib_input/evently/bib_input/_init/selectors/form/submit.js
+++ couchapps/bib_input/evently/bib_input/_init/selectors/form/submit.js
@@ -1,7 +1,8 @@
 function() {
   var form = this;
   var bib = form.bib.value; // we are sure it's an integer because of the regexp check.
-  if (! isBib(bib)) {
+  var app = $$(this).app;
+  if (! isBib(bib) || (app.site_id == undefined)) {
     return false;
   }
 
@@ -10,7 +11,7 @@ function() {
 
   // Send the data to the server and tell the items widget what we want to be the next selected item.
   bib = parseInt(bib, 10);
-  add_checkpoint(bib, $$(this).app, null, function(lap) {
+  add_checkpoint(bib, app, null, function(lap) {
     $(form).trigger('set_selected_item', { bib: bib, lap: lap });
   });
 
