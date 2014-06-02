531f69ade33b4c5420ca011d4bd94fbdf1a91182
Jon Harper <jon.harper87@gmail.com>
2014-06-02 14:50:20 +0200

    BUGFIX: fix search when clicking on a tr of multiple contestants

--- couchapps/search/evently/contestant-display/multiple-contestants/selectors/tr/click.js
+++ couchapps/search/evently/contestant-display/multiple-contestants/selectors/tr/click.js
@@ -2,6 +2,7 @@ function(e) {
   $(this).trigger("single-contestant", {
     name: this.getAttribute('data-name'),
     first_name: this.getAttribute('data-firstname'),
+    race: parseInt(this.getAttribute('data-race')),
     bib: parseInt(this.getAttribute('data-bib'))
   });
 }
