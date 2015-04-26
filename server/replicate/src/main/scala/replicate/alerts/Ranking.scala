760c6de3c47d21bc5e8849e0cc96c28d9f7d2b4f
Samuel Tardieu <sam@rfc1149.net>
2015-04-26 23:42:37 +0200

    Do not use limit while computing ranking

--- server/replicate/src/main/scala/replicate/alerts/Ranking.scala
+++ server/replicate/src/main/scala/replicate/alerts/Ranking.scala
@@ -12,9 +12,7 @@ object Ranking {
   def headOfRace(raceId: Int, database: Database)(implicit ec: ExecutionContext): Future[Option[Int]] = {
     Global.infos.flatMap(_.races.get(raceId)).fold(Future.successful(None: Option[Int])) { raceInfo =>
       val response = database.list("main_display", "global-ranking", "global-ranking",
-        Seq("limit" -> "1",
-          "startkey" -> Json.stringify(Json.arr(raceId, -raceInfo.laps)),
-          "endkey" -> Json.stringify(Json.arr(raceId + 1))))
+        Seq("startkey" -> Json.stringify(Json.arr(raceId, -raceInfo.laps)), "endkey" -> Json.stringify(Json.arr(raceId + 1))))
       response.filter(_.status.isSuccess).map { r =>
         val result = Json.parse(r.entity.asString(HttpCharsets.`UTF-8`))
         (result \\ "id").headOption.map { id =>
