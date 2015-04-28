fc5adaa3fe87bf5d6809ad30ef0e1a9b7b828e19
Samuel Tardieu <sam@rfc1149.net>
2015-04-28 15:19:21 +0200

    Filter by raceId only

--- server/replicate/src/main/scala/replicate/alerts/Ranking.scala
+++ server/replicate/src/main/scala/replicate/alerts/Ranking.scala
@@ -12,7 +12,7 @@ object Ranking {
   def headOfRace(raceId: Int, database: Database)(implicit fm: FlowMaterializer, ec: ExecutionContext): Future[Option[Int]] = {
     Global.infos.flatMap(_.races.get(raceId)).fold(Future.successful(None: Option[Int])) { raceInfo =>
       val response = database.list("main_display", "global-ranking", "global-ranking",
-        Seq("startkey" -> Json.stringify(Json.arr(raceId, -raceInfo.laps)), "endkey" -> Json.stringify(Json.arr(raceId + 1))))
+        Seq("startkey" -> Json.stringify(Json.arr(raceId)), "endkey" -> Json.stringify(Json.arr(raceId + 1))))
       response.filter(_.status.isSuccess).flatMap(r => Couch.jsonUnmarshaller[JsValue]().apply(r.entity)).map { result =>
         (result \\ "id").headOption.map { id =>
           // id is of the form checkpoints-CHECKPOINT-CONTESTANT
