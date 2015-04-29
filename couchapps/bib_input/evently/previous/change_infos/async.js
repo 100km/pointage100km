7e860823e1e2e6e80e1973782d80ee690cbcfaba
Samuel Tardieu <sam@rfc1149.net>
2015-04-29 11:10:41 +0200

    Fix typo

--- couchapps/bib_input/evently/previous/change_infos/async.js
+++ couchapps/bib_input/evently/previous/change_infos/async.js
@@ -1,6 +1,6 @@
 function(cb, e, data) {
   var app = $$(this).app;
-  if (data.race == 5) { //5 is "relai course par equipe team"
+  if (data.race == 5) { //5 is "relais course par equipe team"
     cb(data);
   } else {
     db_previous(app, app.site_id, data, cb);
