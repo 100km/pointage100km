268d5e82404b3143805cb07b7fa98c9f4b59bba3
Samuel Tardieu <sam@rfc1149.net>
2016-02-25 16:59:18 +0100

    Do not be redundant

--- server/replicate/src/main/scala/replicate/maintenance/PingService.scala
+++ server/replicate/src/main/scala/replicate/maintenance/PingService.scala
@@ -4,7 +4,6 @@ import akka.actor.{Actor, FSM}
 import akka.event.Logging
 import net.rfc1149.canape._
 import play.api.libs.json.JsObject
-import replicate.Replicate
 import replicate.utils.{LoggingError, Options}
 
 import scala.concurrent.duration._
@@ -19,7 +18,8 @@ class PingService(options: Options.Config, db: Database) extends Actor with FSM[
   when(0, stateTimeout = 30 seconds) {
 
     case Event(StateTimeout, _) =>
-      if (Replicate.options.ping)
+      if (options.ping)
+
         withError(steenwerck.ping(db, options.siteId), "cannot ping database")
       stay()
 
