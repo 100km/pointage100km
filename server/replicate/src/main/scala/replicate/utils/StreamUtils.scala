11bf2c0e2134c9f1fad22c1439871989bccdb8b4
Samuel Tardieu <sam@rfc1149.net>
2016-02-28 17:27:37 +0100

    New stream utilities

--- server/replicate/src/main/scala/replicate/utils/StreamUtils.scala
+++ server/replicate/src/main/scala/replicate/utils/StreamUtils.scala
@@ -2,6 +2,8 @@ package replicate.utils
 
 import akka.NotUsed
 import akka.stream.scaladsl.Flow
+import akka.stream.stage._
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
 
 import scala.concurrent.duration.{Deadline, FiniteDuration}
 
@@ -28,6 +30,125 @@ trait StreamUtils {
       }
       filter
     }
+
+  private class IdleAlert[T](timeout: FiniteDuration, alert: T) extends GraphStage[FlowShape[T, T]] {
+
+    val in: Inlet[T] = Inlet("IdleAlert.in")
+    val out: Outlet[T] = Outlet("IdleAlert.out")
+
+    override def createLogic(inheritedAttributes: Attributes) = new TimerGraphStageLogic(shape) {
+
+      var element: Option[T] = None
+      var signalled: Boolean = false
+
+      setHandler(in, new InHandler {
+        override def onPush() = {
+          if (!isClosed(out)) {
+            push(out, grab(in))
+            cancelTimer(None)
+            signalled = false
+          } else
+            element = Some(grab(in))
+        }
+      })
+
+      setHandler(out, new OutHandler {
+        override def onPull() =
+          if (element.isDefined) {
+            push(out, element.get)
+            element = None
+            cancelTimer(None)
+            signalled = false
+          } else {
+            if (!hasBeenPulled(in))
+              pull(in)
+            if (!signalled)
+              scheduleOnce(None, timeout)
+          }
+
+        override def onDownstreamFinish() =
+          cancelTimer(None)
+      })
+
+      override protected def onTimer(timerKey: Any): Unit =
+        if (!isClosed(out)) {
+          signalled = true
+          push(out, alert)
+        }
+
+      (in, out)
+    }
+
+    override def shape = FlowShape(in, out)
+  }
+
+  /**
+    * Insert an alert element into a stream if the downstream demand is not satisfied during
+    * the specified timeout. After the alert has been inserted into the stream, it will not
+    * be inserted again until an upstream element flows through.
+    *
+    * @param timeout the timeout after which the alert element is inserted
+    * @param alert the alert element
+    * @tparam T the type of stream elements
+    * @return the enriched stream
+    */
+  def idleAlert[T](timeout: FiniteDuration, alert: T): Flow[T, T, NotUsed] =
+    Flow[T].via(new IdleAlert[T](timeout, alert))
+
+  /**
+    * Return sliding pairs of upstream after filtering out duplicates.
+    *
+    * @tparam T the type of stream elements
+    * @return the stream of paired elements
+    */
+  def pairDifferent[T]: Flow[T, (T, T), NotUsed] =
+    Flow[T].sliding(2).collect { case Seq(a, b) if a != b => (a, b) }
+
+  private class OnlyIncreasing[T](strict: Boolean)(implicit ord: Ordering[T]) extends GraphStage[FlowShape[T, T]] {
+
+    private[this] val in: Inlet[T] = Inlet("OnlyIncreasing.in")
+    private[this] val out: Outlet[T] = Outlet("OnlyIncreasing.out")
+
+    override def createLogic(inheritedAttributes: Attributes) = new GraphStageLogic(shape) {
+
+      private[this] val ordCriterium: Int = if (strict) -1 else 0
+      private[this] var previousValue: Option[T] = None
+
+      setHandler(in, new InHandler {
+        override def onPush() = {
+          val value = grab(in)
+          previousValue match {
+            case None =>
+              push(out, value)
+              previousValue = Some(value)
+            case Some(previous) if ord.compare(previous, value) <= ordCriterium =>
+              push(out, value)
+              previousValue = Some(value)
+            case _ =>
+              pull(in)
+          }
+        }
+      })
+
+      setHandler(out, new OutHandler {
+        override def onPull() = pull(in)
+      })
+    }
+
+    override def shape = FlowShape(in, out)
+  }
+
+  /**
+    * Filter upstream so that it only contains increasing elements according to the specified
+    * ordering.
+    *
+    * @param strict `true` if the comparison must be strict, `false` if equal elements can be emitted
+    * @param ord the Ordering instance to use for comparison
+    * @tparam T the type of stream elements
+    * @return the stream of increasing elements
+    */
+  def onlyIncreasing[T](strict: Boolean)(implicit ord: Ordering[T]): Flow[T, T, NotUsed] =
+    Flow[T].via(new OnlyIncreasing[T](strict)(ord))
 }
 
 object StreamUtils extends StreamUtils
--- server/replicate/src/test/scala/StreamUtilsSpec.scala
+++ server/replicate/src/test/scala/StreamUtilsSpec.scala
@@ -19,9 +19,9 @@ class StreamUtilsSpec extends Specification {
   "sample()" should {
 
     "filter the right number of elements" in new withActorSystem {
-      val source = Source.tick(0.second, 50.milliseconds, 1).zipWith(Source(1 to 100)) { case (_, n) => n }
-      // This will get the samples at 0, 100, 150, and 200 milliseconds, namely 1, 3, 5, and 7
-      val result = source.via(sample(70.milliseconds)).take(4).runWith(Sink.reduce[Int](_+_))
+      val source = Source.tick(0.second, 100.milliseconds, 1).zipWith(Source(1 to 100)) { case (_, n) => n }
+      // This will get the samples at 0, 200, 300, and 040 milliseconds, namely 1, 3, 5, and 7
+      val result = source.via(sample(140.milliseconds)).take(4).runWith(Sink.reduce[Int](_+_))
       Await.result(result, 1.second) must be equalTo 16
     }
 
@@ -41,6 +41,83 @@ class StreamUtilsSpec extends Specification {
       Await.result(result, 2.seconds) must throwA[RuntimeException]("marker")
     }
 
+  }
+
+  "idleAlert()" should {
+
+    "not trigger when upstream progresses fast enough" in new withActorSystem {
+      val result = Source.tick(0.second, 50.milliseconds, 1).via(idleAlert[Int](100.milliseconds, 100))
+        .take(5).runWith(Sink.reduce[Int](_+_))
+      Await.result(result, 2.seconds) must be equalTo 5
+    }
+
+    "trigger exactly once per alert period" in new withActorSystem {
+      val result = Source.tick(0.second, 50.milliseconds, 1).via(idleAlert[Int](10.milliseconds, 100))
+        .take(5).runWith(Sink.fold(List[Int]())(_ :+ _))
+      Await.result(result, 2.seconds) must be equalTo List(1, 100, 1, 100, 1)
+    }
+  }
+
+  "pairDifferent()" should {
+
+    "return successive differences in pairs" in new withActorSystem {
+      val result = Source(List("one", "two", "two", "two", "three")).via(pairDifferent).runFold[List[(String, String)]](Nil)(_ :+ _)
+      Await.result(result, 2.seconds) must be equalTo List(("one", "two"), ("two", "three"))
+    }
+
+    "work with an empty stream" in new withActorSystem {
+      val result = Source.empty[String].via(pairDifferent).runFold[List[(String, String)]](Nil)(_ :+ _)
+      Await.result(result, 2.seconds) must be equalTo Nil
+    }
+
+    "return an empty stream for an one-element stream" in new withActorSystem {
+      val result = Source.single("one").via(pairDifferent).runFold[List[(String, String)]](Nil)(_ :+ _)
+      Await.result(result, 2.seconds) must be equalTo Nil
+    }
+
+    "return an empty stream for a constant stream of elements" in new withActorSystem {
+      val result = Source.repeat("constant").take(5).via(pairDifferent).runFold[List[(String, String)]](Nil)(_ :+ _)
+      Await.result(result, 2.seconds) must be equalTo Nil
+    }
+  }
+
+  "onlyIncreasing" should {
+
+    "filter out non-increasing elements" in new withActorSystem {
+      val result = Source(List(1, 2, 3, 0, 0, 4, -4, 5, 5, -5, 4, 3)).via(onlyIncreasing(strict = true)).runFold[List[Int]](Nil)(_ :+ _)
+      Await.result(result, 2.seconds) must be equalTo List(1, 2, 3, 4, 5)
+    }
+
+    "support specifying an alternate ordering" in new withActorSystem {
+      val ordering = new Ordering[Int] {
+        override def compare(x: Int, y: Int) = implicitly[Ordering[Int]].compare(y, x)
+      }
+      val result = Source(List(1, 2, 3, 0, 0, 4, -4, 5, 5, -5, 4, 3)).via(onlyIncreasing(strict = true)(ordering)).runFold[List[Int]](Nil)(_ :+ _)
+      Await.result(result, 2.seconds) must be equalTo List(1, 0, -4, -5)
+    }
+
+    "keep duplicates in non-strict mode" in new withActorSystem {
+      val result = Source(List(1, 2, 3, 0, 0, 4, -4, 5, 5, -5, 4, 3)).via(onlyIncreasing(strict = false)).runFold[List[Int]](Nil)(_ :+ _)
+      Await.result(result, 2.seconds) must be equalTo List(1, 2, 3, 4, 5, 5)
+    }
+
+    "keep duplicates in non-strict mode with alternate ordering" in new withActorSystem {
+      val ordering = new Ordering[Int] {
+        override def compare(x: Int, y: Int) = implicitly[Ordering[Int]].compare(y, x)
+      }
+      val result = Source(List(1, 2, 3, 0, 0, 4, -4, 5, 5, -5, 4, 3)).via(onlyIncreasing(strict = false)(ordering)).runFold[List[Int]](Nil)(_ :+ _)
+      Await.result(result, 2.seconds) must be equalTo List(1, 0, 0, -4, -5)
+    }
+
+    "transform an empty stream into an empty stream" in new withActorSystem {
+      val result = Source.empty[Int].via(onlyIncreasing(strict = true)).runFold[List[Int]](Nil)(_ :+ _)
+      Await.result(result, 2.seconds) must be equalTo Nil
+    }
+
+    "transform a one-element stream into a one-element stream" in new withActorSystem {
+      val result = Source.single(42).via(onlyIncreasing(strict = true)).runFold[List[Int]](Nil)(_ :+ _)
+      Await.result(result, 2.seconds) must be equalTo List(42)
+    }
   }
 
 }
