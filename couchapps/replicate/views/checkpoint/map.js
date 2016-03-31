d9ffd30ceebe49da538c1f3f3c66f0f459451056
Samuel Tardieu <sam@rfc1149.net>
2016-03-31 16:56:58 +0200

    Fix analyzer lockup

--- couchapps/replicate/views/checkpoint/map.js
+++ couchapps/replicate/views/checkpoint/map.js
@@ -1,4 +1,4 @@
 function(doc) {
-  if (doc.type == "checkpoint" && doc.bib && doc.site_id && doc.race_id != undefined && doc.times != undefined)
+  if (doc.type == "checkpoint" && doc.bib && doc.site_id != undefined && doc.race_id != undefined && doc.times != undefined)
     emit(doc.bib, doc);
 }
