d49c7d14b04733cf956ad3078b96e4f707b3bd47
Samuel Tardieu <sam@rfc1149.net>
2016-04-05 22:59:28 +0200

    Use replication filters

---
+++ couchapps/replicate/filters/to-download.js
@@ -0,0 +1,21 @@
+function(doc, req) {
+  // Those are already strings, keep them as strings
+  var site_id = req.query.site_id;
+  var prev_site_id = req.query.prev_site_id;
+
+  var startsWith = function(prefix) {
+    return doc._id.substring(0, prefix.length) === prefix;
+  };
+
+  if (doc.scope === "local")
+    return false;
+
+  return doc._id === "infos" ||
+    doc._id === "configuration" ||
+    doc._id === "couchsync" ||
+    (site_id !== undefined && startsWith("checkpoints-" + site_id + "-")) ||
+    (prev_site_id !== undefined && startsWith("checkpoints-" + prev_site_id + "-")) ||
+    startsWith("contestant-") ||
+    startsWith("_design/") ||
+    startsWith("message-");
+}
