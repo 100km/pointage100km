19bf263e53f657ec377906fabebb1321186fe375
Samuel Tardieu <sam@rfc1149.net>
2016-04-05 18:15:52 +0200

    RankingState tests

---
+++ server/replicate/src/test/scala/replicate/RankingStateSpec.scala
@@ -0,0 +1,50 @@
+package replicate
+
+import akka.stream.scaladsl.{Sink, Source}
+import org.specs2.mutable._
+import play.api.libs.json.Json
+import replicate.scrutineer.Analyzer
+import replicate.state.{CheckpointsState, PingState, RankingState}
+import replicate.utils.{Global, Infos}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class RankingStateSpec extends Specification {
+
+  import Global.{dispatcher, flowMaterializer}
+
+  sequential
+
+  "RankingState" should {
+
+    val infos: Infos = Json.parse(classOf[ClassLoader].getResourceAsStream("/infos.json")).as[Infos]
+    Global.infos = Some(infos)
+    (0 to 6).foreach(PingState.setLastPing(_, System.currentTimeMillis()))
+    RaceUtils.installFullRace(pristine = true)
+
+    def first10(raceId: Int): Vector[Int] = {
+      val inserts = Source.fromFuture(CheckpointsState.contestants(raceId))
+        .flatMapConcat(Source(_))
+        .mapAsync(4)(contestantId ⇒ CheckpointsState.timesFor(raceId, contestantId).map(Analyzer.analyze(raceId, contestantId, _)))
+        .mapAsync(16)(RankingState.enterAnalysis)
+        .runWith(Sink.ignore)
+      Await.ready(inserts, 5.seconds)
+      val race = Await.result(RankingState.rankingsFor(raceId), 1.second)
+      race.take(10).map(_.contestantId)
+    }
+
+    "get the right final order in race 1" in {
+      first10(1) must be equalTo Vector(688, 24, 201, 719, 522, 242, 241, 110, 314, 217)
+    }
+
+    "get the right final order in race 2" in {
+      first10(2) must be equalTo Vector(393, 491, 56, 490, 71, 91, 287, 670, 933, 560)
+    }
+
+    "get the right final order in race 3" in {
+      first10(3) must be equalTo Vector(879, 867, 738, 498, 280, 735, 788, 790, 701, 759)
+    }
+  }
+
+}
