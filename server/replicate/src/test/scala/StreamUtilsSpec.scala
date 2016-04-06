250d2c2fe896e6e4d1aaf63c2229d20628b53b44
Samuel Tardieu <sam@rfc1149.net>
2016-04-06 17:08:44 +0200

    Properly rename test environment

--- server/replicate/src/test/scala/StreamUtilsSpec.scala
+++ server/replicate/src/test/scala/StreamUtilsSpec.scala
@@ -12,7 +12,7 @@ import scala.concurrent.duration._
 
 class StreamUtilsSpec extends Specification {
 
-  trait withActorSystem extends After {
+  trait WithActorSystem extends After {
     // FIXME: some things are odds with fuzzing mode
     /*
     val config = ConfigFactory.parseString(
@@ -30,25 +30,25 @@ class StreamUtilsSpec extends Specification {
 
   "sample()" should {
 
-    "filter the right number of elements" in new withActorSystem {
+    "filter the right number of elements" in new WithActorSystem {
       val source = Source.tick(0.second, 100.milliseconds, 1).zipWith(Source(1 to 100)) { case (_, n) ⇒ n }
       // This will get the samples at 0, 200, 300, and 040 milliseconds, namely 1, 3, 5, and 7
       val result = source.via(sample(140.milliseconds)).take(4).runWith(Sink.reduce[Int](_ + _))
       Await.result(result, 1.second) must be equalTo 16
     }
 
-    "propagate an upstream completion without delay" in new withActorSystem {
+    "propagate an upstream completion without delay" in new WithActorSystem {
       val result = Source.single(1).via(sample(1.second)).runWith(Sink.reduce[Int](_ + _))
       Await.result(result, 100.milliseconds) must be equalTo 1
     }
 
-    "propagate an upstream error without delay" in new withActorSystem {
+    "propagate an upstream error without delay" in new WithActorSystem {
       val source = Source.single(1) ++ Source.failed(new RuntimeException("marker"))
       val result = source.via(sample(1.second)).runWith(Sink.reduce[Int](_ + _))
       Await.result(result, 100.milliseconds) must throwA[RuntimeException]("marker")
     }
 
-    "propagate an initial error" in new withActorSystem {
+    "propagate an initial error" in new WithActorSystem {
       val result = Source.failed[Int](new RuntimeException("marker")).via(sample(1.second)).runWith(Sink.head)
       Await.result(result, 2.seconds) must throwA[RuntimeException]("marker")
     }
@@ -64,20 +64,20 @@ class StreamUtilsSpec extends Specification {
       try Thread.sleep(duration.toMillis)
       catch { case _: InterruptedException ⇒ }
 
-    "let a single element go through" in new withActorSystem {
+    "let a single element go through" in new WithActorSystem {
       val (upstream, downstream) = probes()
       upstream.sendNext("foobar").sendComplete()
       downstream.request(2).expectNoMsg(50.milliseconds).expectNext("foobar").expectComplete()
     }
 
-    "propagate errors immediately" in new withActorSystem {
+    "propagate errors immediately" in new WithActorSystem {
       val (upstream, downstream) = probes()
       val error = new RuntimeException("random error")
       upstream.sendNext("foobar").sendError(error)
       downstream.request(2).expectError(error)
     }
 
-    "let elements flow through in order if keys are distincts" in new withActorSystem {
+    "let elements flow through in order if keys are distincts" in new WithActorSystem {
       val (upstream, downstream) = probes()
       upstream.sendNext("foo")
       delay(10.milliseconds)
@@ -87,7 +87,7 @@ class StreamUtilsSpec extends Specification {
       downstream.request(4).expectNoMsg(50.milliseconds).expectNext("foo", "bar", "xyzzy").expectComplete()
     }
 
-    "replace elements with the same key" in new withActorSystem {
+    "replace elements with the same key" in new WithActorSystem {
       val (upstream, downstream) = probes()
       upstream.sendNext("foo")
       delay(10.milliseconds)
@@ -99,7 +99,7 @@ class StreamUtilsSpec extends Specification {
       downstream.request(5).expectNoMsg(50.milliseconds).expectNext("bar", "final", "xyzzy").expectComplete()
     }
 
-    "do not replace elements with the same key if the delay has expired" in new withActorSystem {
+    "do not replace elements with the same key if the delay has expired" in new WithActorSystem {
       val (upstream, downstream) = probes()
       upstream.sendNext("foo")
       delay(50.milliseconds)
@@ -114,13 +114,13 @@ class StreamUtilsSpec extends Specification {
 
   "idleAlert()" should {
 
-    "not trigger when upstream progresses fast enough" in new withActorSystem {
+    "not trigger when upstream progresses fast enough" in new WithActorSystem {
       val result = Source.tick(0.second, 50.milliseconds, 1).via(idleAlert[Int](100.milliseconds, 100))
         .take(5).runWith(Sink.reduce[Int](_ + _))
       Await.result(result, 2.seconds) must be equalTo 5
     }
 
-    "trigger exactly once per alert period" in new withActorSystem {
+    "trigger exactly once per alert period" in new WithActorSystem {
       val result = Source.tick(0.second, 1.second, NotUsed).zip(Source.fromIterator(() ⇒ Iterator.from(0))).map(_._2)
         .via(idleAlert[Int](200.milliseconds, 100))
         .take(5).runWith(Sink.fold(List[Int]())(_ :+ _))
@@ -130,27 +130,27 @@ class StreamUtilsSpec extends Specification {
 
   "pairDifferent()" should {
 
-    "return successive differences in pairs" in new withActorSystem {
+    "return successive differences in pairs" in new WithActorSystem {
       val downstream = Source(List("one", "two", "two", "two", "three")).via(pairDifferent).runWith(TestSink.probe)
       downstream.request(3).expectNext(("one", "two"), ("two", "three")).expectComplete()
     }
 
-    "work with an empty stream" in new withActorSystem {
+    "work with an empty stream" in new WithActorSystem {
       val downstream = Source.empty[String].via(pairDifferent).runWith(TestSink.probe)
       downstream.request(1).expectComplete()
     }
 
-    "return an empty stream for an one-element stream" in new withActorSystem {
+    "return an empty stream for an one-element stream" in new WithActorSystem {
       val downstream = Source.single("one").via(pairDifferent).runWith(TestSink.probe)
       downstream.request(1).expectComplete()
     }
 
-    "return an empty stream for a constant stream of elements" in new withActorSystem {
+    "return an empty stream for a constant stream of elements" in new WithActorSystem {
       val downstream = Source.repeat("constant").take(5).via(pairDifferent).runWith(TestSink.probe)
       downstream.request(1).expectComplete()
     }
 
-    "propagate an error" in new withActorSystem {
+    "propagate an error" in new WithActorSystem {
       val (upstream, downstream) = TestSource.probe[Int].via(pairDifferent).toMat(TestSink.probe)(Keep.both).run()
       // downstream.request(1)
       upstream.sendNext(1).sendNext(2).sendNext(3)
@@ -162,12 +162,12 @@ class StreamUtilsSpec extends Specification {
 
   "onlyIncreasing" should {
 
-    "filter out non-increasing elements" in new withActorSystem {
+    "filter out non-increasing elements" in new WithActorSystem {
       val downstream = Source(List(1, 2, 3, 0, 0, 4, -4, 5, 5, -5, 4, 3)).via(onlyIncreasing(strict = true)).runWith(TestSink.probe)
       downstream.request(6).expectNext(1, 2, 3, 4, 5).expectComplete()
     }
 
-    "support specifying an alternate ordering" in new withActorSystem {
+    "support specifying an alternate ordering" in new WithActorSystem {
       val ordering = new Ordering[Int] {
         override def compare(x: Int, y: Int) = implicitly[Ordering[Int]].compare(y, x)
       }
@@ -175,12 +175,12 @@ class StreamUtilsSpec extends Specification {
       downstream.request(5).expectNext(1, 0, -4, -5).expectComplete()
     }
 
-    "keep duplicates in non-strict mode" in new withActorSystem {
+    "keep duplicates in non-strict mode" in new WithActorSystem {
       val downstream = Source(List(1, 2, 3, 0, 0, 4, -4, 5, 5, -5, 4, 3)).via(onlyIncreasing(strict = false)).runWith(TestSink.probe)
       downstream.request(7).expectNext(1, 2, 3, 4, 5, 5).expectComplete()
     }
 
-    "keep duplicates in non-strict mode with alternate ordering" in new withActorSystem {
+    "keep duplicates in non-strict mode with alternate ordering" in new WithActorSystem {
       val ordering = new Ordering[Int] {
         override def compare(x: Int, y: Int) = implicitly[Ordering[Int]].compare(y, x)
       }
@@ -188,12 +188,12 @@ class StreamUtilsSpec extends Specification {
       downstream.request(6).expectNext(1, 0, 0, -4, -5).expectComplete()
     }
 
-    "transform an empty stream into an empty stream" in new withActorSystem {
+    "transform an empty stream into an empty stream" in new WithActorSystem {
       val downstream = Source.empty[Int].via(onlyIncreasing(strict = true)).runWith(TestSink.probe)
       downstream.request(1).expectComplete()
     }
 
-    "transform a one-element stream into a one-element stream" in new withActorSystem {
+    "transform a one-element stream into a one-element stream" in new WithActorSystem {
       val downstream = Source.single(42).via(onlyIncreasing(strict = true)).runWith(TestSink.probe)
       downstream.request(2).expectNext(42).expectComplete()
     }
