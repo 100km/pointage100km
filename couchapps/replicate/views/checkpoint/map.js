6d8b4bdb0a7c92e1952b9b6a5ef8add9f53c0037
Samuel Tardieu <sam@rfc1149.net>
2016-04-07 19:11:48 +0200

    Do not included deleted checkpoints if any

--- couchapps/replicate/views/checkpoint/map.js
+++ couchapps/replicate/views/checkpoint/map.js
@@ -1,5 +1,5 @@
 function(doc) {
   // FIXME: check for doc.type == "checkpoint" when issue #102 is fixed
-  if (doc.bib && doc.site_id != undefined && doc.race_id != undefined && doc.times != undefined)
+  if (doc.bib && doc.site_id != undefined && doc.race_id != undefined && doc.times != undefined && !doc._deleted)
     emit(doc.bib, doc);
 }
