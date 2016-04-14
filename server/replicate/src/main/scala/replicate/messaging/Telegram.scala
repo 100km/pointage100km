ff50fe0d3b9cc3e17846cf3d5e137a7319cbbd37
Samuel Tardieu <sam@rfc1149.net>
2016-04-14 20:04:38 +0200

    Remove extra logging

--- server/replicate/src/main/scala/replicate/messaging/Telegram.scala
+++ server/replicate/src/main/scala/replicate/messaging/Telegram.scala
@@ -9,10 +9,6 @@ import replicate.utils.Global
 import scala.concurrent.Future
 
 class Telegram(id: String) extends Actor with ActorLogging with Messaging {
-  override def preStart = {
-    super.preStart()
-    log.info(s"Telegram service starting for id $id")
-  }
 
   override def sendMessage(message: Message): Future[Option[String]] = {
     val mdUrl = message.url.fold("")(uri ⇒ s" [(link)]($uri)")
