6c3bc9007c020d8d44529e1acf3766531671a6bd
Samuel Tardieu <sam@rfc1149.net>
2016-04-14 23:56:21 +0200

    Experiment with docker-compose

--- docker/start.sh
+++ docker/start.sh
@@ -1,18 +1,10 @@
 #! /bin/sh
 #
 
-start_couchdb() {
-  while true; do
-    /entrypoint.sh couchdb
-    echo CouchDB crashed 2>&1
-    sleep 5
-  done
-}
-
 start_replicate() {
   cd /home/steenwerck
   while true; do
-     gosu steenwerck pointage100km/bin/replicate "$@"
+     pointage100km/bin/replicate "$@"
      if [ $? = 0 ]; then
        echo Replicate exited with status 0
        # Let couchdb some time to persist the data in any case
@@ -26,15 +18,8 @@ start_replicate() {
 
 if [ "$1" = replicate ]; then
   shift
-  echo Starting couchdb and replicate "$@"
-  start_couchdb &
-  sleep 5
+  echo Starting replicate "$@"
   start_replicate "$@"
-fi
-
-if [ "$1" = couchdb ]; then
-  echo Starting couchdb
-  start_couchdb
 fi
 
 exec "$@"
