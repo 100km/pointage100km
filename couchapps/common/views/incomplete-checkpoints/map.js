df191a94c461bd0b839b7bba340f5149a4a41302
Samuel Tardieu <sam@rfc1149.net>
2016-04-15 11:59:28 +0200

    Use new utilities to solve conflicts

--- couchapps/common/views/incomplete-checkpoints/map.js
+++ couchapps/common/views/incomplete-checkpoints/map.js
@@ -1,7 +1,7 @@
 // View incomplete-checkpoints
 // Used in scala in order to fix contectant with race 0
-function(doc) {
-  if (doc.times && doc.times.length > 0 && doc.race_id == 0) {
-    emit(null, doc);
+function (doc) {
+  if (doc.type === "checkpoint" && doc.times && doc.times.length > 0 && doc.race_id === 0) {
+    emit(doc._rev, doc);
   }
-};
+}
