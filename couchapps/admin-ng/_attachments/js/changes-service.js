e19075b0518a8e23a4060ee299e3d7d331a0cd2b
Samuel Tardieu <sam@rfc1149.net>
2016-04-18 15:31:08 +0200

    Use since=now for live changes

--- couchapps/admin-ng/_attachments/js/changes-service.js
+++ couchapps/admin-ng/_attachments/js/changes-service.js
@@ -1,41 +1,38 @@
-angular.module("admin-ng").factory("changesService", ["database", "$httpParamSerializer", function(database, $httpParamSerializer) {
-  return {
-    onChange: function(scope, params, callback) {
-      var ev;
-      var allParams = angular.merge({feed: "eventsource"}, params);
+angular.module("admin-ng").factory("changesService", ["database", "$httpParamSerializer",
+    function(database, $httpParamSerializer) {
+      return {
+        onChange: function(scope, params, callback) {
+          var ev;
+          var allParams = angular.merge({feed: "eventsource"}, params);
 
-      var reconnect = function() {
-        ev = new EventSource(database + "/_changes?" + $httpParamSerializer(allParams));
-        ev.onmessage = function(event) {
-          allParams.since = event.lastEventId;
-          scope.$applyAsync(function() { callback(JSON.parse(event.data)); });
-        };
-        ev.onerror = function(event) {
-          if (ev.readyState === EventSource.CLOSED) {
-            ev.close();
-            reconnect();
-          }
-        };
+          var reconnect = function() {
+            ev = new EventSource(database + "/_changes?" + $httpParamSerializer(allParams));
+            ev.onmessage = function(event) {
+              allParams.since = event.lastEventId;
+              scope.$applyAsync(function() { callback(JSON.parse(event.data)); });
+            };
+            ev.onerror = function(event) {
+              if (ev.readyState === EventSource.CLOSED) {
+                ev.close();
+                reconnect();
+              }
+            };
+          };
+
+          reconnect();
+          scope.$on("$destroy", function() { ev.close(); });
+        }
       };
-
-      reconnect();
-      scope.$on("$destroy", function() { ev.close(); });
-    }
-  };
-}]);
+    }]);
 
 angular.module("admin-ng").factory("globalChangesService", ["changesService", "$rootScope", "database", "$http",
     function(changesService, $rootScope, database, $http) {
       return {
         start: function() {
-          $http.get(database).then(function(response) {
-            var status = response.data;
-            var sequence = status.update_seq;
-            changesService.onChange($rootScope, {since: sequence, heartbeat: 3000, include_docs: true},
-                function(change) {
-                  $rootScope.$broadcast("change", change);
-                });
-          });
+          changesService.onChange($rootScope, {since: "now", heartbeat: 3000, include_docs: true},
+              function(change) {
+                $rootScope.$broadcast("change", change);
+              });
         }
       };
     }]);
