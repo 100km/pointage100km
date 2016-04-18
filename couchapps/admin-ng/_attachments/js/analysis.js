80168df5b17fc14942c0ba8296615a241703bf98
Samuel Tardieu <sam@rfc1149.net>
2016-04-18 11:39:46 +0200

    Attempt at better styling

--- couchapps/admin-ng/_attachments/js/analysis.js
+++ couchapps/admin-ng/_attachments/js/analysis.js
@@ -52,16 +52,29 @@ angular.module("admin-ng").component("analysisSummary", {
 
 angular.module("admin-ng").controller("analysisPoint", ["$scope", "dbService",
     function($scope, dbService) {
+      var setAction = function(label, icon, clazz) {
+        $scope.action_label = label;
+        $scope.action_icon = icon;
+        $scope.action_class = clazz;
+      };
       $scope.site = "Site " + $scope.point.site_id;
-      $scope.action_icon = "trash";
       $scope.action_label = "Remove";
+      $scope.action_icon = "trash";
       $scope.action_class = "danger";
       if ($scope.point.action === "add") {
-        $scope.action_label = "Add";
-        $scope.action_icon = "plus-sign";
-        $scope.action_class = "success";
-      } else if ($scope.point.action === undefined) {
-        $scope.action_class = "default";
+        setAction("Add", "plus-sign", "success");
+        $scope.tooltip = "The algorithm suggests to add this point";
+      } else if ($scope.point.action === "remove") {
+        setAction("Remove", "trash", "danger");
+        $scope.tooltip = "The algorithm suggests to delete this point";
+      } else if ($scope.point.type === "deleted") {
+        setAction("Restore", "plus-sign", "deleted");
+        $scope.tooltip = "This time has been previously deleted";
+      } else if ($scope.point.type === "artificial") {
+        $scope.tooltip = "This time has been inserted manually";
+        setAction("Remove", "remove", "artificial");
+      } else {
+        setAction("Remove", "trash", "default");
       }
       dbService.infos.then(function(infos) {
         $scope.site = infos.sites[$scope.point.site_id] + " (" + $scope.point.site_id + ")";
