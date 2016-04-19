c8db1247547bece16545eb078adb9f24064ac50a
Samuel Tardieu <sam@rfc1149.net>
2016-04-19 13:30:07 +0200

    Reorganize code around listing

--- couchapps/admin-ng/_attachments/js/analysis.js
+++ couchapps/admin-ng/_attachments/js/analysis.js
@@ -63,31 +63,25 @@ angular.module("admin-ng").component("analysisSummary", {
 
 angular.module("admin-ng").controller("analysisPoint", ["$scope", "dbService",
     function($scope, dbService) {
-      var setAction = function(label, icon, clazz) {
+      var setAction = function(label, icon, clazz, tooltip) {
         $scope.action_label = label;
         $scope.action_icon = icon;
-        $scope.action_class = clazz;
+        $scope.action_class = clazz || $scope.point.type;
+        $scope.tooltip = tooltip;
       };
       $scope.site = "Site " + $scope.point.site_id;
-      $scope.action_label = "Remove";
-      $scope.action_icon = "trash";
-      $scope.action_class = "danger";
       if ($scope.point.action === "add") {
-        setAction("Add", "plus-sign", "success");
-        $scope.tooltip = "The algorithm suggests to add this point";
+        setAction("Add", "plus-sign", "success", "The algorithm suggests to add this point");
       } else if ($scope.point.action === "remove") {
-        setAction("Remove", "trash", "danger");
-        $scope.tooltip = "The algorithm suggests to delete this point";
+        setAction("Remove", "trash", "danger", "The algorithm suggests to delete this point");
       } else if ($scope.point.type === "deleted") {
-        setAction("Restore", "plus-sign", "deleted");
-        $scope.tooltip = "This time has been previously deleted";
+        setAction("Restore", "plus-sign", "deleted", "This time has been previously deleted");
       } else if ($scope.point.type === "down") {
-        setAction(null, null, "down");
+        setAction();
       } else if ($scope.point.type === "artificial") {
-        $scope.tooltip = "This time has been inserted manually";
-        setAction("Remove", "remove", "artificial");
+        setAction("Remove", "remove", "artificial", "This time has been inserted manually");
       } else {
-        setAction("Remove", "trash", "default");
+        setAction("Remove", "trash");
       }
       dbService.infos.then(function(infos) {
         $scope.site = infos.sites[$scope.point.site_id] + " (" + $scope.point.site_id + ")";
