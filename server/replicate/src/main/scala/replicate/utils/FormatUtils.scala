5a09c12e6d6c7f7fcb8ee4deba0523c607fe3228
Samuel Tardieu <sam@rfc1149.net>
2016-04-19 22:41:10 +0200

    Honor race timezone when building messages

--- server/replicate/src/main/scala/replicate/utils/FormatUtils.scala
+++ server/replicate/src/main/scala/replicate/utils/FormatUtils.scala
@@ -1,12 +1,13 @@
 package replicate.utils
 
-import java.util.Calendar
+import java.util.{Calendar, TimeZone}
 
 object FormatUtils {
 
   def formatDate(timestamp: Long, withSeconds: Boolean = false) = {
     val calendar = Calendar.getInstance()
     calendar.setTimeInMillis(timestamp)
+    calendar.setTimeZone(TimeZone.getTimeZone(Global.infos.get.timezone));
     if (withSeconds)
       "%d:%02d:%02d".format(calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND))
     else
