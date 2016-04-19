b034c724f1b93ad045a60dd751a2558cf5967d86
Samuel Tardieu <sam@rfc1149.net>
2016-04-19 14:55:11 +0200

    Use components and directives

--- couchapps/admin-ng/_attachments/js/analysis.js
+++ couchapps/admin-ng/_attachments/js/analysis.js
@@ -1,9 +1,16 @@
-function AnalysisListController($scope, $http, database) {
-  var $ctrl = this;
-  $http.get(database + "/_design/admin-ng/_view/by-anomalies")
-    .then(function(response) {
-      $scope.anomalies = response.data.rows.map(function(o) { return o.value; });
-    });
+//
+// Analysis list
+//
+
+function AnalysisListController($http, database) {
+  var ctrl = this;
+
+  this.$onInit = function() {
+    return $http.get(database + "/_design/admin-ng/_view/by-anomalies")
+      .then(function(response) {
+        ctrl.anomalies = response.data.rows.map(function(o) { return o.value; });
+      });
+  };
 }
 
 angular.module("admin-ng").component("analysisList", {
@@ -11,42 +18,50 @@ angular.module("admin-ng").component("analysisList", {
   controller: AnalysisListController
 });
 
-function AnalysisController($scope, dbService, changesService) {
-  var $ctrl = this;
-  $scope.needsFixing = false;
+//
+// Analysis for one contestant
+//
 
-  $scope.loadAnalysis = function() {
-    dbService.enrichedAnalysis($scope.bib).then(function(analysis) {
-      $scope.analysis = analysis;
-      $scope.needsFixing = analysis.anomalies > 0;
+function AnalysisController($scope, dbService, changesService) {
+  var ctrl = this;
+  ctrl.needsFixing = false;
+
+  ctrl.loadAnalysis = function() {
+    return dbService.enrichedAnalysis(ctrl.bib).then(function(analysis) {
+      ctrl.analysis = analysis;
+      ctrl.needsFixing = analysis.anomalies > 0;
     });
   };
 
   this.$routerOnActivate = function(next, previous) {
-    $scope.bib = Number(next.params.bib);
+    ctrl.bib = Number(next.params.bib);
 
-    dbService.infos.then(function(infos) { $scope.infos = infos; });
-    $scope.loadAnalysis();
+    dbService.infos.then(function(infos) { ctrl.infos = infos; });
 
     // If either the document or the contestant information changes,
     // we want to reload a fresh analysis.
-    changesService.onChange($scope,
-        {filter: "_doc_ids", doc_ids: '["contestant-' + $scope.bib + '","analysis-' + $scope.bib + '"]',
-          heartbeat: 30000, /* since: "now" */},
-          $scope.loadAnalysis);
+    changesService.onChange($scope, {
+      filter: "_doc_ids",
+      doc_ids: '["contestant-' + ctrl.bib + '","analysis-' + ctrl.bib + '"]',
+      heartbeat: 30000, since: "now"
+    },
+    ctrl.loadAnalysis);
+
+    // Wait until we have loaded the initial analysis before continuing.
+    return ctrl.loadAnalysis();
   };
 }
 
 angular.module("admin-ng").component("analysis", {
   templateUrl: "partials/analysis.html",
   controller: AnalysisController,
-  bindings: {
-    bib: '<'
-  }
 });
 
-function AnalysisSummaryController($scope) {
-  var $ctrl = this;
+//
+// Analysis summary (or before/after)
+//
+
+function AnalysisSummaryController() {
 }
 
 angular.module("admin-ng").component("analysisSummary", {
@@ -57,36 +72,78 @@ angular.module("admin-ng").component("analysisSummary", {
     points: '<',
     analysis: '<',
     active: '<',
-    panelid: '<'
+    infos: '<',
+    addAction: '&',
   }
 });
 
-angular.module("admin-ng").controller("analysisPoint", ["$scope", "dbService",
-    function($scope, dbService) {
-      var setAction = function(label, icon, clazz, tooltip) {
-        $scope.action_label = label;
-        $scope.action_icon = icon;
-        $scope.action_class = clazz || $scope.point.type;
-        $scope.tooltip = tooltip;
+//
+// Represent a given point (table row) in the analysis
+//
+
+function AnalysisPointController($scope) {
+  var ctrl = $scope;
+  this.$onInit = function() {
+    ctrl.site = ctrl.infos.sites[ctrl.point.site_id] + " (" + ctrl.point.site_id + ")";
+
+    if (ctrl.active) {
+      var setDisplay = function(label, icon, clazz, tooltip) {
+        ctrl.action_label = label;
+        ctrl.action_icon = icon;
+        ctrl.action_class = clazz || ctrl.point.type;
+        ctrl.tooltip = tooltip;
       };
-      $scope.site = "Site " + $scope.point.site_id;
-      if ($scope.point.action === "add") {
-        setAction("Add", "plus-sign", "success", "The algorithm suggests to add this point");
-      } else if ($scope.point.action === "remove") {
-        setAction("Remove", "trash", "danger", "The algorithm suggests to delete this point");
-      } else if ($scope.point.type === "deleted") {
-        setAction("Restore", "plus-sign", "deleted", "This time has been previously deleted");
-      } else if ($scope.point.type === "down") {
-        setAction();
-      } else if ($scope.point.type === "artificial") {
-        setAction("Remove", "remove", "artificial", "This time has been inserted manually");
+      var setAction = function(action, isSuggested) {
+        ctrl.action = {docid: "checkpoints-" + ctrl.point.site_id + "-" + ctrl.bib,
+          action: action, time: ctrl.point.time};
+        if (isSuggested) {
+          // FIXME: Add to global actions here
+        }
+      };
+      ctrl.act = function() {
+        console.log("Performing action " + JSON.stringify(ctrl.action));
+      };
+      if (ctrl.point.action === "add") {
+        setDisplay("Add", "plus-sign", "success", "The algorithm suggests to add this point");
+        setAction("add", true);
+      } else if (ctrl.point.action === "remove") {
+        setDisplay("Remove", "trash", "danger", "The algorithm suggests to delete this point");
+        setAction("remove", true);
+      } else if (ctrl.point.type === "deleted") {
+        setDisplay("Restore", "plus-sign", "deleted", "This time has been previously deleted");
+        setAction("add");
+      } else if (ctrl.point.type === "down") {
+        setDisplay();
+      } else if (ctrl.point.type === "artificial") {
+        setDisplay("Remove", "remove", "artificial", "This time has been inserted manually");
+        setAction("remove");
       } else {
-        setAction("Remove", "trash");
+        setDisplay("Remove", "trash");
+        setAction("remove");
       }
-      dbService.infos.then(function(infos) {
-        $scope.site = infos.sites[$scope.point.site_id] + " (" + $scope.point.site_id + ")";
-      });
-    }]);
+    }
+  };
+
+}
+
+angular.module("admin-ng").directive("analysisPoint", function() {
+  return {
+    templateUrl: "partials/analysis-point.html",
+    controller: AnalysisPointController,
+    restrict: "A",
+    replace: true,
+    scope: {
+      infos: "<",
+      point: "<",
+      addAction: "&",
+      active: "<"
+    }
+  };
+});
+
+//
+// Routing component
+//
 
 angular.module("admin-ng").component("analysisTop", {
   template: "<ng-outlet></ng-outlet>",
