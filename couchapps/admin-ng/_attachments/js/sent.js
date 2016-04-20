cdbc958d226c69ef43724b7c6eac9a6584bbe3a6
Samuel Tardieu <sam@rfc1149.net>
2016-04-20 17:02:49 +0200

    Display SMS

---
+++ couchapps/admin-ng/_attachments/js/sent.js
@@ -0,0 +1,14 @@
+function SentController($scope, changesService) {
+  var ctrl = this;
+  this.messages = [];
+
+  changesService.initThenOnChange($scope, "replicate", "sms-distance",
+      function(row) {
+        ctrl.messages.push(row.doc);
+      });
+}
+
+angular.module("admin-ng").component("sent", {
+  templateUrl: "partials/sent-sms.html",
+  controller: SentController,
+});
