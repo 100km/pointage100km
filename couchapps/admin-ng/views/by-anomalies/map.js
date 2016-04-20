c75b20f9867b94ad3465011899b8aa2607d4c096
Samuel Tardieu <sam@rfc1149.net>
2016-04-20 16:25:10 +0200

    Use database to remember sent SMS

--- couchapps/admin-ng/views/by-anomalies/map.js
+++ couchapps/admin-ng/views/by-anomalies/map.js
@@ -1,4 +1,4 @@
-function (doc, req) {
+function (doc) {
   if (doc.type == "analysis") {
     emit([doc.valid, -doc.anomalies], {"race_id": doc.race_id, "anomalies": doc.anomalies, "valid": doc.valid, "bib": doc.bib});
   }
