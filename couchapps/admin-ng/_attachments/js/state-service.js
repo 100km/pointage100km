08481c63cba79ef3642b8a9038bfbbbe59d24a8c
Samuel Tardieu <sam@rfc1149.net>
2016-04-20 10:22:13 +0200

    Better display

--- couchapps/admin-ng/_attachments/js/state-service.js
+++ couchapps/admin-ng/_attachments/js/state-service.js
@@ -1,32 +1,67 @@
-function StateService($http, database, changesService, $rootScope) {
+angular.module("admin-ng").factory("stateService",
+    ["$http", "database", "changesService", "$rootScope",
+    function($http, database, changesService, $rootScope) {
 
-  $service = this;
+      var data = {
+        contestants: {},
+      };
 
-  this.contestants = {};
+      changesService.onChange($rootScope, {
+        heartbeat: 30000, since: 0, filter: "_view", view: "common/all_contestants",
+        include_docs: true
+      }, function (change) {
+        var contestant = change.doc;
+        $rootScope.$applyAsync(function() { data.contestants[contestant.bib] = contestant; });
+      });
 
-  this.injectContestants = function($scope) {
-    $scope.contestants = $service.contestants;
-  };
+      changesService.onChange($rootScope, {
+        heartbeat: 30000, since: 0, filter: "_doc_ids", doc_ids: '["infos"]', include_docs: true
+      }, function (change) {
+        $rootScope.$applyAsync(function() { data.infos = change.doc; });
+      });
 
-  changesService.onChange($rootScope, {
-    heartbeat: 30000, since: 0, filter: "_view", view: "common/all_contestants",
-    include_docs: true
-  }, function (change) {
-    var contestant = change.doc;
-    contestant.full_name = contestant.first_name + " " + contestant.name +
-      " (bib " + contestant.bib + ")";
-    $rootScope.$applyAsync(function() { $service.contestants[contestant.bib] = contestant; });
-  });
-}
+      return data;
+
+}]);
 
 angular.module("admin-ng")
-        .service("stateService", StateService)
         .directive("contestant", function() {
           return {
-            template: "{{contestants[bib] ? contestants[bib].full_name : 'Bib ' + bib}}",
+            template: "{{contestantName}}",
             scope: { bib: "<" },
             controller: function($scope, stateService) {
-              stateService.injectContestants($scope);
+              this.$onInit = function() {
+                $scope.$watchGroup([function() { return stateService.contestants[$scope.bib]; },
+                                    "bib"],
+                    function(values) {
+                      var contestant = values[0];
+                      if (contestant)
+                        $scope.contestantName = contestant.first_name + " " + contestant.name +
+                          " (bib " + contestant.bib + ")";
+                      else
+                        $scope.contestantName = "Bib " + $scope.bib;
+                    });
+              };
+            }
+          };
+        })
+        .directive("race", function() {
+          return {
+            template: "{{raceName}}",
+            scope: { raceId: "<" },
+            controller: function($scope, stateService) {
+              this.$onInit = function() {
+                $scope.$watchGroup([function() { return stateService.infos; },
+                                    "raceId"],
+                    function(values) {
+                      var infos = values[0];
+                      var raceId = values[1];
+                      if (infos)
+                        $scope.raceName = infos.races_names[raceId];
+                      else
+                        $scope.raceName = "Race " + raceId;
+                    });
+              };
             }
           };
         });
