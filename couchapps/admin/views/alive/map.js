80228745002a543ef9e484498f884462555d91ca
Samuel Tardieu <sam@rfc1149.net>
2016-04-20 12:46:24 +0200

    Protect against incomplete documents

--- couchapps/admin/views/alive/map.js
+++ couchapps/admin/views/alive/map.js
@@ -2,11 +2,12 @@ function(doc) {
   if (doc.type === "ping")
     emit(doc.site_id, doc.time);
   else if (doc.type === "checkpoint") {
+    var times = doc.times || [];
     var artificial_times = doc.artificial_times || [];
-    var i = doc.times.length - 1;
+    var i = times.length - 1;
     while (i >= 0) {
-      if (artificial_times.indexOf(doc.times[i]) === -1) {
-        emit(doc.site_id, doc.times[i]);
+      if (artificial_times.indexOf(times[i]) === -1) {
+        emit(doc.site_id, times[i]);
         break;
       }
       i--;
