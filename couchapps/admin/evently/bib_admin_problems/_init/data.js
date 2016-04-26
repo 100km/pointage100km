9f6b0826180996f98a00cd5385ccf86f295b241d
Jon Harper <jon.harper87@gmail.com>
2016-04-26 23:28:52 +0200

    couchapps, admin, send timestamps instead of dates; fixes #104

--- couchapps/admin/evently/bib_admin_problems/_init/data.js
+++ couchapps/admin/evently/bib_admin_problems/_init/data.js
@@ -1,4 +1,27 @@
 function(data) {
+
+  function format_date(time) {
+    date = new Date(time);
+    return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
+  }
+
+  for (var i=0; i<data.pbs.length ;i++) {
+    for (var j=0; j<data.pbs[i].sites.length; j++) {
+      for (var k=0; k<data.pbs[i].sites[j].times.length; k++) {
+        data.pbs[i].sites[j].times[k].formatted_val = format_date(data.pbs[i].sites[j].times[k].val);
+      }
+      for (var k=0; k<data.pbs[i].sites[j].artificial_times.length; k++) {
+        data.pbs[i].sites[j].artificial_times[k].formatted_val = format_date(data.pbs[i].sites[j].artificial_times[k].val);
+      }
+      for (var k=0; k<data.pbs[i].sites[j].deleted_times.length; k++) {
+        data.pbs[i].sites[j].deleted_times[k].formatted_val = format_date(data.pbs[i].sites[j].deleted_times[k].val);
+      }
+    }
+    for (var j=0; j<data.pbs[i].times.length; j++) {
+      data.pbs[i].times[j].formatted_time = format_date(data.pbs[i].times[j].time);
+    }
+  }
+
   // Return the data.
   return data;
 };
