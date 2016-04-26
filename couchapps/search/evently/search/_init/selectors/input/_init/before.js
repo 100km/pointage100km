6580c58c8ae3942b7085f60ddf26d55bd291ba20
Jon Harper <jon.harper87@gmail.com>
2016-04-26 22:31:57 +0200

    couchapp, search, autosubmit form

--- couchapps/search/evently/search/_init/selectors/input/_init/before.js
+++ couchapps/search/evently/search/_init/selectors/input/_init/before.js
@@ -2,6 +2,10 @@ function() {
   var app = $$(this).app;
   $(this).autocomplete({
     html: true,
+    select: function(event, ui) {
+      $("#search-input").val(ui.item.value);
+      $("#search-form").submit();
+    },
     source: function(request, response) {
       if (!isNaN(parseInt(request.term))) {
         response([]);
