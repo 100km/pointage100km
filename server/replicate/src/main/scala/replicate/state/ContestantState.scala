c439820a6e5c3b568009c37191dc2b0ec9c3a630
Samuel Tardieu <sam@rfc1149.net>
2016-04-26 18:42:49 +0200

    Fix stalkers acquisition

--- server/replicate/src/main/scala/replicate/state/ContestantState.scala
+++ server/replicate/src/main/scala/replicate/state/ContestantState.scala
@@ -23,13 +23,13 @@ object ContestantState {
         contestantAgent.alter(_ ⇒ rows.map { case (_, contestant) ⇒ contestant.contestantId → contestant }.toMap)
         log.info("ContestantAgent: initial state loaded")
         // Then check for contestants changes
-        database.changesSource(Map("filter" → "_view", "_view" → "common/all_contestants"), sinceSeq = seq)
+        database.changesSource(Map("filter" → "_view", "view" → "common/all_contestants", "include_docs" → "true"), sinceSeq = seq)
           .mapAsyncUnordered(1) { js ⇒
             (js \ "doc").validate[Contestant] match {
               case JsSuccess(contestant, _) ⇒
                 contestantAgent.alter(m ⇒ m + (contestant.contestantId → contestant))
-              case JsError(_) ⇒
-                log.error("unable to analyze document {}", (js \ "id").as[String])
+              case JsError(error) ⇒
+                log.error("unable to analyze document {}: {}", (js \ "id").as[String], error)
                 FastFuture.successful(NotUsed)
             }
           }
