d0d557783728d32735c5522f0b1e3c1a965d4a89
Samuel Tardieu <sam@rfc1149.net>
2016-04-26 13:06:27 +0200

    Support alerts mass insertion without mass requesting
    However, to prevent a bug in CouchDB where a mass insertion in parallel
    with mass queries using "include_docs=true" will sometimes return a null
    document.

--- couchapps/admin-ng/_attachments/js/alerts.js
+++ couchapps/admin-ng/_attachments/js/alerts.js
@@ -18,18 +18,35 @@ function AlertsController($scope, changesService, stateService, dbService) {
   this.currentPage = 1;
   this.itemsPerPage = 20;
 
-  this.loadAlerts = () => {
+  this.loadAlerts = () =>
         dbService.getAlertsFrom((this.currentPage - 1) * this.itemsPerPage, this.itemsPerPage)
           .then(response => {
             this.totalItems = response.data.total_rows;
-            this.alerts = response.data.rows.map(row => row.doc);
+            this.alerts = response.data.rows.map(row => row.value);
           });
+
+  // Ensure that not more than one reload is active at the same time. Newer reload triggers
+  // will be executed when this one is finished.
+  this.loading = false;
+  this.mustReload = false;
+  this.reloadAlerts = () => {
+    if (this.loading)
+      this.mustReload = true;
+    else {
+      this.loading = true;
+      this.mustReload = false;
+      this.loadAlerts().then(() => {
+        this.loading = false;
+        if (this.mustReload)
+          this.reloadAlerts();
+      });
+    }
   };
 
   changesService.filterChanges($scope, change => change.doc.type === "alert",
-      () => this.loadAlerts());
+      () => this.reloadAlerts());
 
-  this.loadAlerts();
+  this.reloadAlerts();
 
 }
 
