ff9b8f22edfb24eaeb5de1e438e18d7fd5598b28
Jon Harper <jon.harper87@gmail.com>
2018-03-13 23:43:20 +0100

    search, don't access the contestant doc directly for privacy

--- couchapps/search/evently/contestant-display/display-contestants/async.js
+++ couchapps/search/evently/contestant-display/display-contestants/async.js
@@ -3,13 +3,20 @@ function(cb, wtf, request) {
 
   var request_int = parseInt(request);
   if (!isNaN(request_int)) {
-    app.db.openDoc(infos_id(request_int), {
+    app.db.view("search/contestants-search", {
       success: function(data) {
-        cb([{value: data}]);
+        var res = data.rows;
+        res.request = request;
+          cb(res);
       },
       error: function() {
-        cb([]);
-      }
+        var res = [];
+        res.request = request;
+        cb(res);
+      },
+      startkey: request_int,
+      endkey: request_int,
+      inclusive_end: true
     });
   } else {
     db_search_str(app, request, function(data) {
