8aad1c1e4e6d1e396bd28dbdce1dfdcf38935e45
Samuel Tardieu <sam@rfc1149.net>
2018-05-09 22:28:41 +0200

    Consider only valid analysis in global rankings

--- couchapps/main_display/views/global-ranking-ng/map.js
+++ couchapps/main_display/views/global-ranking-ng/map.js
@@ -2,7 +2,7 @@
 // Used for global ranking of all contestants (to be diplayed in the web site for example)
 //TODO teams
 function(doc) {
-  if (doc.type === "analysis" && doc.after.length > 0) {
+  if (doc.type === "analysis" && doc.after.length > 0 && doc.valid) {
     var last = doc.after[doc.after.length - 1];
     var synthesized = {bib: doc.bib, race_id: doc.race_id, site_id: last.site_id,
       times: [last.time]};
